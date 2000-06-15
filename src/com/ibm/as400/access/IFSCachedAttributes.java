7ad90269b13128335aaf4a9a43eced5b74393b1b
John Eberhard <jeber@us.ibm.com>
2000-06-15 20:14:36 +0000

    Mod4 codebase refresh

---
+++ src/com/ibm/as400/access/IFSCachedAttributes.java
@@ -0,0 +1,137 @@
+///////////////////////////////////////////////////////////////////////////////
+//                                                                             
+// AS/400 Toolbox for Java - OSS version                                       
+//                                                                             
+// Filename: IFSCachedAttributes.java
+//                                                                             
+// The source code contained herein is licensed under the IBM Public License   
+// Version 1.0, which has been approved by the Open Source Initiative.         
+// Copyright (C) 1997-2000 International Business Machines Corporation and     
+// others. All rights reserved.                                                
+//                                                                             
+///////////////////////////////////////////////////////////////////////////////
+
+package com.ibm.as400.access;
+import java.io.Serializable;
+
+
+/**
+Store cached attributes.
+**/
+class IFSCachedAttributes implements Serializable 
+{
+  private static final String copyright = "Copyright (C) 1997-2000 International Business Machines Corporation and others.";
+
+  static final int FA_READONLY  = 0x01;                               
+  static final int FA_HIDDEN    = 0x02;                              
+
+  long accessDate_;
+  long creationDate_;
+  int fixedAttributes_;
+  boolean isDirectory_;
+  boolean isFile_;
+  long modificationDate_;
+  String name_;
+  int objectType_;
+  String path_;
+  int size_;  
+
+/**
+Construct listCachedAttributes object from a list of attributes.
+**/
+  IFSCachedAttributes(long accessDate, long creationDate, int fixedAttributes, 
+                      long modificationDate, int objectType, int size, 
+                      String name, String path, boolean isDirectory, boolean isFile)
+  {
+    accessDate_ = accessDate;
+    creationDate_ = creationDate;
+    fixedAttributes_ = fixedAttributes;
+    isDirectory_ = isDirectory;
+    isFile_ = isFile;
+    modificationDate_ = modificationDate;
+    name_ =  name;
+    objectType_ = objectType;
+    path_ = path;
+    size_ = size;
+  }
+
+/**
+Return access date.
+**/
+  long getAccessDate()
+  {
+      return accessDate_;
+  }
+
+/**
+Return creation date.
+**/
+  long getCreationDate()
+  {
+      return creationDate_;
+  }
+
+/**
+Return fixed attributes.
+**/
+  int getFixedAttributes()
+  {
+      return fixedAttributes_;
+  }
+
+/**
+Return isDir_
+**/
+  boolean getIsDirectory()
+  {
+      return isDirectory_;
+  }
+
+/**
+Return fixed attributes.
+**/
+  boolean getIsFile()
+  {
+      return isFile_;
+  }
+
+/**
+Return modification date.
+**/
+  long getModificationDate()
+  {
+      return modificationDate_;
+  }
+
+/**
+Return name.
+**/
+  String getName()
+  {
+      return name_;
+  }
+
+/**
+Return object type.
+**/
+  int getObjectType()
+  {
+      return objectType_;
+  }
+
+/**
+Return path.
+**/
+  String getPath()
+  {
+      return path_;
+  }
+
+/**
+Return size.
+**/
+  int getSize()
+  {
+      return size_;
+  }
+}
--- src/com/ibm/as400/access/IFSChangeAttrsReq.java
+++ src/com/ibm/as400/access/IFSChangeAttrsReq.java
@@ -13,8 +13,6 @@
 
 package com.ibm.as400.access;
 
-import java.io.IOException;
-import java.io.InputStream;
 
 /**
 Change attributes request.
@@ -39,7 +37,8 @@ class IFSChangeAttrsReq extends IFSDataStreamReq
   private static final int TEMPLATE_LENGTH = 48;
 
 /**
-Construct a change attributes request.
+Construct a change attributes request.  Use this request to change
+the size of the file by file handle (the file is open)
 @param fileHandle handle of file to change
 @param fileSize the desired file size in bytes
 **/
@@ -61,7 +60,8 @@ Construct a change attributes request.
   }
 
 /**
-Construct a change attributes request.
+Construct a change attributes request.  Use this form to change the
+date/time stamps of the file by handle (the file is open).
 @param fileHandle handle of file to change
 @param createDate the desired creation date (measured in milliseconds since
 January 1, 1970 00:00:00 GMT)
@@ -89,7 +89,8 @@ January 1, 1970 00:00:00 GMT)
   }
 
 /**
-Construct a change attributes request.
+Construct a change attributes request.  Use this form to change
+the size of the file by file name.
 @param fileName the name of the file to change
 @param fileNameCCSID file name CCSID
 @param fileSize the desired file size in bytes
@@ -123,7 +124,49 @@ Construct a change attributes request.
   }
 
 /**
-Construct a change attributes request.
+Construct a change attributes request.  Use this form to change the fixed attributes
+of the file.
+@param fileName the name of the file to change
+@param fileNameCCSID file name CCSID
+@param fixedAttributes the fixed attributes to set
+@param stuff          an extra boolean that is ignored.  It is needed
+                      so the signature of this constructor is unique.
+**/
+  IFSChangeAttrsReq(byte[]  fileName,                                      //@D1a
+                    int     fileNameCCSID,                                 //@D1a
+                    int     fixedAttributes,                               //@D1a
+                    boolean extraneousDataToMakeSignatureUnique)           //@D1a
+  {                                                                        //@D1a
+    super(20 + TEMPLATE_LENGTH + 6 + fileName.length);                     //@D1a
+    setLength(data_.length);                                               //@D1a
+    setTemplateLen(TEMPLATE_LENGTH);                                       //@D1a
+    setReqRepID(0x000b);                                                   //@D1a
+    set32bit(0, FILE_HANDLE_OFFSET);                                       //@D1a
+    set16bit(fileNameCCSID, CCSID_OFFSET);                                 //@D1a
+    set32bit(1, WORKING_DIR_HANDLE_OFFSET);                                //@D1a
+    set16bit(1, ATTR_LIST_LEVEL_OFFSET);                                   //@D1a
+    setData(0L, CREATE_DATE_OFFSET);                                       //@D1a
+    setData(0L, MODIFY_DATE_OFFSET);                                       //@D1a
+    setData(0L, ACCESS_DATE_OFFSET);                                       //@D1a
+    set16bit(2, SET_FLAGS_OFFSET);                                         //@D1a
+    set32bit(fixedAttributes, FIXED_ATTRS_OFFSET);                         //@D1a
+    set32bit(0, FILE_SIZE_OFFSET);                                         //@D1a
+                                                                           //@D1a
+    // Set the LL.                                                         //@D1a
+    set32bit(fileName.length + 6, FILE_NAME_LL_OFFSET);                    //@D1a
+                                                                           //@D1a
+    // Set the code point.                                                 //@D1a
+    set16bit(0x0002, FILE_NAME_CP_OFFSET);                                 //@D1a
+                                                                           //@D1a
+    // Set the file name characters.                                       //@D1a
+    System.arraycopy(fileName, 0, data_, FILE_NAME_OFFSET, fileName.length);  //@D1a
+  }                                                                        //@D1a
+                                                                           //@D1a
+                                                                           //@D1a
+
+/**
+Construct a change attributes request.  Use this form to change the
+date/time stamp of the file by file name.
 @param fileName the name of the file to change
 @param fileNameCCSID file name CCSID
 @param createDate the desired creation date (measured in milliseconds since
@@ -162,11 +205,6 @@ January 1, 1970 00:00:00 GMT)
     System.arraycopy(fileName, 0, data_, FILE_NAME_OFFSET, fileName.length);
   }
 
-  // Get the copyright.
-  private static String getCopyright()
-  {
-    return Copyright.copyright;
-  }
 }
 
 
