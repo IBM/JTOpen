93072a7a7e6c9ce025c49f217ec13e74aa071991
John Eberhard <jeber@us.ibm.com>
2001-06-12 18:07:49 +0000

    Updated copyright for JTOpen 2.03.

--- src/com/ibm/as400/access/Copyright.java
+++ src/com/ibm/as400/access/Copyright.java
@@ -1,12 +1,12 @@
 ///////////////////////////////////////////////////////////////////////////////
 //                                                                             
-// JTOpen (AS/400 Toolbox for Java - OSS version)                              
+// JTOpen (IBM Toolbox for Java - OSS version)                              
 //                                                                             
 // Filename: Copyright.java
 //                                                                             
 // The source code contained herein is licensed under the IBM Public License   
 // Version 1.0, which has been approved by the Open Source Initiative.         
-// Copyright (C) 1997-2000 International Business Machines Corporation and     
+// Copyright (C) 1997-2001 International Business Machines Corporation and     
 // others. All rights reserved.                                                
 //                                                                             
 ///////////////////////////////////////////////////////////////////////////////
@@ -15,20 +15,11 @@ package com.ibm.as400.access;
 
 /**
 The Copyright interface is used to hold the copyright string and the
-version information for the AS/400 Toolbox for Java.
+version information for the IBM Toolbox for Java.
 **/
-// @D0C
-// Implementation notes:
-//
-// All classes in the package need a copyright and should get that
-// by declaring a constant variable as:
-// <PRE>
-// static final private String x = Copyright.copyright;
-// </PRE>
-//
 public interface Copyright
 {
     public static final String copyright = "Copyright (C) 1997-2001 International Business Machines Corporation and others.";
-    public static final String version   = "Open Source Software, JTOpen 2.02, original codebase 5769-JC1 V4R5M0.1";
+    public static final String version   = "Open Source Software, JTOpen 2.03, original codebase 5769-JC1 V4R5M0.1";
 }
 
