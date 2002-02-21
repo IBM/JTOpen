94f5a7300ba446ab60909dd59c63e2cb524c4ec1
John Eberhard <jeber@us.ibm.com>
2002-02-21 18:23:17 +0000

    This commit was generated by cvs2svn to compensate for changes in r364, which included commits to RCS files with non-trunk default branches.

---
+++ micro/com/ibm/as400/micro/ConnectionHandler.java
@@ -0,0 +1,432 @@
+///////////////////////////////////////////////////////////////////////////////
+//                                                                             
+// JTOpen (IBM Toolbox for Java - OSS version)                                 
+//                                                                             
+// Filename: ConnectionHandler.java
+//                                                                             
+// The source code contained herein is licensed under the IBM Public License   
+// Version 1.0, which has been approved by the Open Source Initiative.         
+// Copyright (C) 1997-2001 International Business Machines Corporation and     
+// others. All rights reserved.                                                
+//                                                                             
+///////////////////////////////////////////////////////////////////////////////
+
+package com.ibm.as400.micro;
+
+import java.io.*;
+import java.sql.*;
+
+import com.ibm.as400.access.Trace;
+
+
+/**
+ *  The ConnectionHandler class is designed to handle all interactions
+ *  needed by the JDBC-ME driver with the JDBC Connection interface.
+ **/
+class ConnectionHandler
+{
+  private static final String copyright = "Copyright (C) 1997-2001 International Business Machines Corporation and others.";
+
+    private JdbcMeService service_;
+    private MicroDataInputStream in_;
+    private MicroDataOutputStream out_;
+    
+    /**
+    Constructor.  Creates a JDBC-ME handler for Connection objects.
+    **/
+    public ConnectionHandler(JdbcMeService jdbcme, MicroDataInputStream in, MicroDataOutputStream out)
+    {
+        service_ = jdbcme;
+        in_ = in;
+        out_ = out;
+    }
+
+
+    /**
+    The process function routes the function id and the Connection
+    to the proper handler.
+    **/
+    public void process(Connection connection, int funcId) throws IOException
+    {
+        switch (funcId)
+        {
+        case MEConstants.CONN_CLOSE:
+            close(connection);
+            break;
+        case MEConstants.CONN_CREATE_STATEMENT:
+            createStatement(connection);
+            break;
+        case MEConstants.CONN_CREATE_STATEMENT2:
+            createStatement2(connection);
+            break;
+        case MEConstants.CONN_PREPARE_STATEMENT:
+            prepareStatement(connection);
+            break;
+        case MEConstants.CONN_SET_AUTOCOMMIT:
+            setAutoCommit(connection);
+            break;
+        case MEConstants.CONN_SET_TRANSACTION_ISOLATION:
+            setTransactionIsolation(connection);
+            break;
+        case MEConstants.CONN_COMMIT:
+            commit(connection);
+            break;
+        case MEConstants.CONN_ROLLBACK:
+            rollback(connection);
+            break;
+        default:
+            // TODO:  This is an exception condition...
+            if (Trace.isTraceOn())
+                Trace.log(Trace.ERROR, "Connection Function ID not recognized - function code: " + funcId);
+            break;
+        }
+    }
+
+
+    /**
+    Closes the Connection object.  This function has the side effect
+    of closing all Statements and ResultSets that are open under the
+    connection as well.  Unlike most of the methods of this class,
+    if an exception occurs while closing the Connection, this method
+    will not report it back to the caller in any way.  It is simply
+    swollowed and a message is logged concerning the failure.
+    <P>
+    The data flow is as follows:
+    <input>
+       - nothing more
+    <output>
+       - nothing more
+    <P>
+ 
+    @param connection    The connection object to close.
+    **/
+    public void close(Connection connection) throws IOException
+    {
+        try
+        {
+            connection.close();
+        }
+        catch (SQLException e)
+        {
+            if (Trace.isTraceOn())
+                Trace.log(Trace.ERROR, "Exception caught trying to close connection object " + connection, e);
+        }
+
+        // Remove it from the vector.
+        service_.removeConnection(connection);
+    }
+                                   
+
+    /**
+    Creates a JDBC Statement object under the given connection
+    and returns a int handle to it to the caller.
+    <P>
+    The data flow is as follows:
+    <input>
+       - nothing more
+    <output>
+       int - the statement handle for the object.
+    <P>
+ 
+    @param connection    The connection object to use.
+    **/
+    public void createStatement(Connection connection) throws IOException
+    {
+        try
+        {
+            Statement s = connection.createStatement();
+
+            // Insert it into the vector
+            service_.addStatement(s);
+
+            // Put the object into our map
+            int objectId = service_.mapObject(s);
+            out_.writeInt(objectId);
+            out_.flush();
+        }
+        catch (SQLException e)
+        {
+            service_.handleException(e);
+        }
+    }    
+
+
+    /**
+    Creates a JDBC Statement object under the given connection
+    and returns a int handle to it to the caller.  The statement
+    object that will get created by this function will be scrollable
+    and updatable.
+    <P>
+    The data flow is as follows:
+    <input>
+       - nothing more
+    <output>
+       int - the statement handle for the object.
+    <P>
+ 
+    @param connection    The connection object to use.
+    **/
+    public void createStatement2(Connection connection) throws IOException
+    {
+        try
+        {
+            // This is the JDBC create statement enhancement.
+            int type = in_.readInt();
+            int concurrency = in_.readInt();
+            Statement s = connection.createStatement(type, concurrency);
+            
+            // Insert it into the vector
+            service_.addStatement(s);
+
+            // Put the object into our map
+            int objectId = service_.mapObject(s);
+            out_.writeInt(objectId);
+            out_.flush();
+        }
+        catch (SQLException e)
+        {
+            service_.handleException(e);
+        }
+    }
+
+
+    /**
+    Creates a JDBC PreparedStatement object under the given connection
+    and returns a int handle to it to the caller.
+    <P>
+    The data flow is as follows:
+    <input>
+       String   - the sql statement to prepare.
+       - nothing more
+    <output>
+       int - the prepared statement handle for the object.
+          int 1 = there is a ResultSet
+             int   - the number of columns in the ResultSet
+             int   - list of all the columns data types.
+          int 0 = there is no Resultset
+    <P>
+ 
+    @param connection    The connection object to use.
+    **/
+    public void prepareStatement(Connection connection) throws IOException
+    {
+        try
+        {
+            // TODO:  We may have to ensure that all the SQL processing happens
+            //        before we try to write anything so that we can put out
+            //        a clean stream....
+            // Read in the SQL statement to prepare.
+            String sql = in_.readUTF();
+            
+            // Create the JDBC PreparedStatement
+            PreparedStatement ps = connection.prepareStatement(sql);
+            
+            // Insert it into the vector
+            service_.addStatement(ps);
+
+            // Put the object into our map
+            int objectId = service_.mapObject(ps);
+            out_.writeInt(objectId);
+            out_.flush();
+
+            ResultSetMetaData rsmd = ps.getMetaData();
+            
+            if (rsmd == null)
+                out_.writeInt(0);
+            else
+            {
+                out_.writeInt(1);
+                int count = rsmd.getColumnCount();
+                out_.writeInt(count);
+                
+                for (int i = 0; i < count; i ++)
+                {
+                    out_.writeInt(rsmd.getColumnType(i + 1));
+                }
+            }
+            out_.flush();
+        }
+        catch (SQLException e)
+        {
+            service_.handleException(e);
+        }
+    }
+
+
+    /**
+    Creates a JDBC PreparedStatement object under the given connection
+    and returns a int handle to it to the caller.  The prepared statement
+    object that will get created by this function will be scrollable
+    and updatable.
+    <P>
+    The data flow is as follows:
+    <input>
+       String   - the sql statement to prepare.
+       - nothing more
+    <output>
+       int - the prepared statement handle for the object.
+          int 1 = there is a ResultSet
+             int   - the number of columns in the ResultSet
+             int   - list of all the columns data types.
+          int 0 = there is no Resultset
+    <P>
+ 
+    @param connection    The connection object to use.
+    **/
+    public void prepareStatement2(Connection connection) throws IOException
+    {
+        try
+        {
+            // TODO:  We may have to ensure that all the SQL processing happens
+            //        before we try to write anything so that we can put out
+            //        a clean stream....
+            // Read in the SQL statement to prepare.
+            String sql = in_.readUTF();
+            
+            // Create the JDBC PreparedStatement
+            PreparedStatement ps = connection.prepareStatement(sql, ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+            // Insert it into the vector
+            service_.addStatement(ps);
+
+            // Put the object into our map
+            int objectId = service_.mapObject(ps);
+            out_.writeInt(objectId);
+            out_.flush();
+
+            ResultSetMetaData rsmd = ps.getMetaData();
+            
+            if (rsmd == null)
+                out_.writeInt(0);
+            else
+            {
+                out_.writeInt(1);
+                int count = rsmd.getColumnCount();
+                out_.writeInt(count);
+                
+                for (int i = 0; i < count; i ++)
+                {
+                    out_.writeInt(rsmd.getColumnType(i + 1));
+                }
+            }
+            out_.flush();
+        }
+        catch (SQLException e)
+        {
+            service_.handleException(e);
+        }
+    }
+
+
+    /**
+    Sets the autocommit attribute for the underlying
+    connection object.
+    <P>
+    The data flow is as follows:
+    <input>
+       boolean - t/f for autocommit setting
+    <output>
+       int - 1/0 for success/failure.
+    <P>
+ 
+    @param connection    The connection object to use.
+    **/
+    public void setAutoCommit(Connection connection) throws IOException
+    {
+        try
+        {
+            boolean b = in_.readBoolean();
+            connection.setAutoCommit(b);
+            out_.writeInt(1);
+            out_.flush();
+        }
+        catch (SQLException e)
+        {
+            service_.handleException(e);
+        }
+    }
+
+
+    /**
+    Sets the transaction isolation level for the underlying
+    connection object.
+    <P>
+    The data flow is as follows:
+    <input>
+       int - the transaction isolation level to set
+    <output>
+       int - 1/0 for success/failure.
+    <P>
+ 
+    @param connection    The connection object to use.
+    **/
+    public void setTransactionIsolation(Connection connection) throws IOException
+    {
+        try
+        {
+            int level = in_.readInt();
+            connection.setTransactionIsolation(level);
+            out_.writeInt(1);
+            out_.flush();
+        }
+        catch (SQLException e)
+        {
+            service_.handleException(e);
+        }
+    }
+
+
+    /**
+    Commits the work that is pending under the current
+    transaction.
+    <P>
+    The data flow is as follows:
+    <input>
+       none
+    <output>
+       int - 1/0 for success/failure.
+    <P>
+ 
+    @param connection    The connection object to use.
+    **/
+    public void commit(Connection connection) throws IOException
+    {
+        try
+        {
+            connection.commit();
+            out_.writeInt(1);
+            out_.flush();
+        }
+        catch (SQLException e)
+        {
+            service_.handleException(e);
+        }
+    }
+
+
+    /**
+    Rolls back the work that is pending under the current
+    transaction.
+    <P>
+    The data flow is as follows:
+    <input>
+       none
+    <output>
+       int - 1/0 for success/failure.
+    <P>
+ 
+    @param connection    The connection object to use.
+    **/
+    public void rollback(Connection connection) throws IOException
+    {
+        try
+        {
+            connection.rollback();
+            out_.writeInt(1);
+            out_.flush();
+        }
+        catch (SQLException e)
+        {
+            service_.handleException(e);
+        }
+    }
+}
---
+++ micro/com/ibm/as400/micro/ProgramCall.java
@@ -0,0 +1,190 @@
+///////////////////////////////////////////////////////////////////////////////
+//                                                                             
+// JTOpen (IBM Toolbox for Java - OSS version)                                 
+//                                                                             
+// Filename: AS400.java
+//                                                                             
+// The source code contained herein is licensed under the IBM Public License   
+// Version 1.0, which has been approved by the Open Source Initiative.         
+// Copyright (C) 1997-2001 International Business Machines Corporation and     
+// others. All rights reserved.                                                
+//                                                                             
+///////////////////////////////////////////////////////////////////////////////
+
+package com.ibm.as400.micro;
+
+import java.io.IOException;
+import java.util.Hashtable;
+import java.util.Enumeration;
+
+/**
+ *  The ProgramCall class allows a user to call an iSeries program and access data returned after the program runs
+ *  from a wireless device.  This class provides a modified subset of the functions available in 
+ *  com.ibm.as400.access.ProgramCall.<p>
+ *  
+ *  Each PCML document must be registered with the MEserver.  The document can be
+ *  registered during runtime when the PCML document name parameter is provided or as an argument when the
+ *  MEServer is started or reconfigured.<p>  The registration is simply telling the server which PCML-defined program(s)
+ *  to run.  <p>
+ *
+ *  A hashtable is used to specify the names and values of the parameters to set.  The key in the hashtable
+ *  is the name of the parameter to set and the key value is the value of the corresponding parameter to set.
+ *  There is also a string array which specifies the name of the PCML output parameter(s) to return.
+ *
+ *  <P>The following example demonstrates the use of Program Call:
+ *  <br>
+ *  <pre>
+ *   // Call programs.
+ *   AS400 system = new AS400("mySystem", "myUserid", "myPwd", "myMEServer");
+ *   
+ *   // See the PCML example in the Toolbox programmer's guide.
+ *   String pcmlName = "qsyrusri.pcml"; // The PCML document describing the program we want to use.
+ *   String apiName = "qsyrusri";
+ *
+ *   Hashtable parametersToSet = new Hashtable();
+ *   parametersToSet.put("qsyrusri.receiverLength", "2048");
+ *   parametersToSet.put("qsyrusri.profileName", "JOHNDOE" };
+ *
+ *   String[] parametersToGet = { "qsyrusri.receiver.userProfile",  
+ *               "qsyrusri.receiver.previousSignonDate", 
+ *               "qsyrusri.receiver.previousSignonTime",
+ *               "qsyrusri.receiver.displaySignonInfo" };
+ *               
+ *   String[] valuesToGet = null;
+ *
+ *   try
+ *   {
+ *       valuesToGet = ProgramCall.run(system, pcmlName, apiName, parametersToSet, parametersToGet);
+ *       
+ *       // Get and display the user profile.
+ *       System.out.println("User profile: " + valuesToGet[0]);
+ *
+ *       // Get and display the date in a readable format.
+ *       char[] c = valuesToGet[1].toCharArray();
+ *       System.out.println("Last Signon Date: " + c[3]+c[4]+"/"+c[5]+c[6]+"/"+c[1]+c[2] );
+ *
+ *       // Get and display the time in a readable format.
+ *       char[] d = valuesToGet[2].toCharArray();
+ *       System.out.println("Last Signon Time: " + d[0]+d[1]+":"+d[2]+d[3]);
+ *
+ *       // Get and display the signon info.
+ *       System.out.println("Signon Info: " + valuesToGet[3] );
+ *   }
+ *   catch (MEException te)
+ *   {
+ *       // Handle the exception.
+ *   }
+ *   catch (IOException ioe)
+ *   {
+ *       // Handle the exception
+ *   }
+ *   
+ *   // Done with the system object.
+ *   system.disconnect();
+ *  </pre>
+ *
+ *  @see com.ibm.as400.access.ProgramCall
+ **/
+public final class ProgramCall
+{
+    /**
+     *  Private ProgramCall constructor.
+     **/
+    private ProgramCall()
+    {  }
+
+
+    /**
+     *  Constructs a ProgramCall object using the specified <i>system</i>, <i>pgmName</i>, <i>parametersToSet</i>, <i>parameterValues</i>, and <i>parametersToGet</i>.
+     *
+     *  @param  system                The system on which to run the program.
+     *  @param  pcmlName           The name of the PCML document.
+     *  @param  program               The name of the program.
+     *  @param  parametersToSet  The hashtable containg the key, which is the name of the program parameter, and the key value, 
+     *                                          which is the corresponding program parameter value..
+     *  @param  parametersToGet  The data returned from the program call.
+     *
+     *  @return  the data requested returned from the program.  An empty array is returned if no data is returned.
+     *
+     *  @exception  IOException  If an error occurs while communicating with the server.
+     *  @exception  MEException  If an error occurs while processing the ToolboxME request.
+     **/
+    public static String[] run(AS400 system, String pcmlName, String program, Hashtable parametersToSet, String[] parametersToGet) throws IOException, MEException
+    {
+        if (system == null)
+            throw new NullPointerException("system");
+        
+        if (pcmlName == null)
+            throw new NullPointerException("pcmlName");
+        
+        if (program == null)
+            throw new NullPointerException("program");
+
+        synchronized(system)
+        {
+            system.connect();
+
+            system.toServer_.writeInt(MEConstants.PROGRAM_CALL);
+
+            system.toServer_.writeUTF(pcmlName);
+            system.toServer_.writeUTF(program);
+
+            // Write the number parameter name/value pairs specified in the hashtable.
+            system.toServer_.writeInt(parametersToSet.size());
+            system.toServer_.flush();
+
+            // Loop through the hashtable and write out the key (parameter name)
+            // and the key value (parameter value)
+            for (Enumeration e = parametersToSet.keys() ; e.hasMoreElements() ;) 
+            {
+                String key = (String)e.nextElement();
+                system.toServer_.writeUTF(key);
+                system.toServer_.writeUTF(parametersToSet.get(key).toString());
+            }
+
+            system.toServer_.flush();
+
+            // Write the number of parameters to get or return to the caller.
+            int numparametersToGet = (parametersToGet == null ? 0 : parametersToGet.length);
+            system.toServer_.writeInt(numparametersToGet);
+            system.toServer_.flush();
+            
+            // Write the parameter names to get.
+            for (int i=0; i<numparametersToGet; ++i)
+            {
+                system.toServer_.writeUTF(parametersToGet[i]);
+            }
+            
+            system.toServer_.flush();
+
+            int registered = system.fromServer_.readInt();
+
+            if (registered == MEConstants.EXCEPTION_OCCURRED)
+            {   
+                int rc = system.fromServer_.readInt();
+                String msg = system.fromServer_.readUTF();
+                throw new MEException(msg,rc);
+            }
+            
+            boolean retVal = system.fromServer_.readBoolean(); // Maybe we can remove this from both the client and server?
+
+            // if the program did not run, then create an MEException out
+            // of the AS400Message sent back from the MEServer.
+            if (!retVal)
+            {
+                int rc = system.fromServer_.readInt();
+               String msg = system.fromServer_.readUTF();
+               throw new MEException(msg,rc);
+            }
+
+            String[] values = new String[numparametersToGet];
+
+            for (int i=0; i<numparametersToGet; ++i)
+            {
+                values[i] = system.fromServer_.readUTF();
+            }
+            
+            return values;
+        }
+    }
+}
