7afb81f6b367a9a2ba909cd6227dc9582345195e
bschulz-oss <bschulz-oss>
2003-04-29 16:20:17 +0000

    Made more information available through the exception traces.

--- src/com/ibm/as400/access/SQLDate.java
+++ src/com/ibm/as400/access/SQLDate.java
@@ -162,11 +162,11 @@ implements SQLData
         }
         catch(NumberFormatException e)
         {
-            JDError.throwSQLException(JDError.EXC_DATA_TYPE_MISMATCH);
+            JDError.throwSQLException(JDError.EXC_DATA_TYPE_MISMATCH, e);
         }
         catch(StringIndexOutOfBoundsException e)
         {
-            JDError.throwSQLException(JDError.EXC_DATA_TYPE_MISMATCH);
+            JDError.throwSQLException(JDError.EXC_DATA_TYPE_MISMATCH, e);
         }
 
         return new Date(calendar.getTime().getTime());
