7de35b6982cfd5b0d53e6c50bc19700aa80290b4
John Eberhard <jeber@us.ibm.com>
2003-07-19 18:01:37 +0000

    Updated for JTOpen 4.1.

--- src/com/ibm/as400/access/Copyright.java
+++ src/com/ibm/as400/access/Copyright.java
@@ -20,6 +20,6 @@ version information for the IBM Toolbox for Java.
 public interface Copyright
 {
     public static final String copyright = "Copyright (C) 1997-2003 International Business Machines Corporation and others.";
-    public static final String version   = "Open Source Software, JTOpen 4.0, codebase 5722-JC1 V5R3M0";
+    public static final String version   = "Open Source Software, JTOpen 4.1, codebase 5722-JC1 V5R3M0";
 }
 
