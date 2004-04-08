b7bc34de4ec7d48e260f0ef25b3c814356a26ea1
jlee-oss <jlee-oss>
2004-04-08 15:50:50 +0000

    Removed unnecessary Xalan dependency in PcmlSAXParser

--- src/com/ibm/as400/data/XPCMLHelper.java
+++ src/com/ibm/as400/data/XPCMLHelper.java
@@ -18,6 +18,7 @@ import java.util.*;
 import javax.xml.transform.*;
 import javax.xml.transform.stream.*;
 import org.xml.sax.*;
+import com.ibm.as400.access.Trace;
 
 // This class contains the transform methods originally added to ProgramCallDocument.
 // They are here so that ProgramCallDocument does not have a direct dependency on the XSL/XML
@@ -65,6 +66,26 @@ class XPCMLHelper
             StreamSource streamIn = new StreamSource(streamSource);
             transformer.transform(streamIn, new StreamResult(streamResult));
      }
+
+
+    /**
+      doSimplifyXSDTransform -- Transform that takes XSD file and transforms it to a more readable form for identifying types and their attributes.
+     **/
+    static void doSimplifyXSDTransform(InputStream streamSource, OutputStream streamResult)
+      throws SAXException, IOException 
+    {
+      try
+      {
+        StreamSource in = new StreamSource(SystemResourceFinder.getXPCMLTransformFile("xpcml_xpcml.xsl"));
+        TransformerFactory tFactory = TransformerFactory.newInstance();
+        Transformer transformer = tFactory.newTransformer(in);
+        transformer.transform(new StreamSource(streamSource), new StreamResult(streamResult));
+      }
+      catch (TransformerException e){
+        Trace.log(Trace.ERROR, e);
+        throw new SAXException(e);
+      }
+    }
 
 
 }
