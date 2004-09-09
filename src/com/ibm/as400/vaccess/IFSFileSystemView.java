8e53cdc1eb593b6aa9088d10a98ca0f2915c73e9
jlee-oss <jlee-oss>
2004-09-09 22:21:10 +0000

    Upgraded for newer JDKs

--- src/com/ibm/as400/vaccess/IFSFileSystemView.java
+++ src/com/ibm/as400/vaccess/IFSFileSystemView.java
@@ -22,6 +22,8 @@ import java.io.IOException;
 import java.text.MessageFormat;
 import javax.swing.filechooser.FileSystemView;
 import javax.swing.UIManager;
+import javax.swing.Icon;
+
 
 /**
   * IFSFileSystemView provides a gateway to the iSeries integrated file system,
@@ -62,6 +64,7 @@ public class IFSFileSystemView extends FileSystemView
 {
     private static final String copyright = "Copyright (C) 1997-2003 International Business Machines Corporation and others.";
 
+    private static final boolean DEBUG = false;
     private AS400 system_;
     private static final String ROOT_PATH = IFSJavaFile.separator;
 
@@ -74,16 +77,17 @@ public class IFSFileSystemView extends FileSystemView
     private static final String NEW_FOLDER_STRING = ((NEW_FOLDER_STRING0 == null || NEW_FOLDER_STRING0.length() == 0) ? DEFAULT_FOLDER_NAME : NEW_FOLDER_STRING0);
     private static final String NEW_FOLDER_NEXT_STRING = ((NEW_FOLDER_NEXT_STRING0 == null || NEW_FOLDER_NEXT_STRING0.length() == 0) ? DEFAULT_FOLDER_NAME+"{0}" : NEW_FOLDER_NEXT_STRING0);
 
+    private static Icon             serverIcon32_           = ResourceLoader.getIcon ("AS40032.gif");
+
+
     /**
      Constructs an IFSFileSystemView object.
      @param system The iSeries system that contains the file.
      **/
     public IFSFileSystemView(AS400 system)
     {
-        if(system == null)
-        {
-            throw new NullPointerException("system");
-        }
+        if (system == null) throw new NullPointerException("system");
+
         system_ = system;
     }
 
@@ -91,22 +95,32 @@ public class IFSFileSystemView extends FileSystemView
      Returns a File object constructed in directory from the given filename.
      <br>Note: This method does not create an actual file in the file system.
      @param containingDir The directory in which to create the file.
-     <br>containingDir must be of type {@link com.ibm.as400.access.IFSJavaFile IFSJavaFile}.
+     <br>containingDir must be of type {@link com.ibm.as400.access.IFSJavaFile IFSJavaFile}.  If null, it is ignored.
      @param name The file name.
      @return a File object representing the new file.
      **/
     public File createFileObject(File containingDir, String name)
     {
-        try
-        {
-            return new IFSJavaFile((IFSJavaFile)containingDir, name);
+      if (name == null) throw new NullPointerException("name");
+
+      IFSJavaFile file;
+
+      if (containingDir != null) {
+        IFSJavaFile ifsDir;
+        if (DEBUG) {
+          ifsDir = convertToIFSJavaFile(containingDir, "createFileObject");
         }
-        catch(ClassCastException e)
-        {
-            Trace.log(Trace.ERROR, "Argument must be of type IFSJavaFile.");
-            throw new ExtendedIllegalArgumentException("containingDir",
-                                                       ExtendedIllegalArgumentException.PARAMETER_VALUE_NOT_VALID);
+        else {
+          ifsDir = convertToIFSJavaFile(containingDir);
         }
+        file = new IFSJavaFile(ifsDir, name);
+      }
+      else {
+        file = new IFSJavaFile(system_, name);
+      }
+
+      if (isFileSystemRoot(file)) return createFileSystemRoot(file);
+      else return file;
     }
 
     /**
@@ -117,7 +131,28 @@ public class IFSFileSystemView extends FileSystemView
      **/
     public File createFileObject(String path)
     {
-        return new IFSJavaFile(system_, path);
+      if (path == null) throw new NullPointerException("path");
+      IFSJavaFile file = new IFSJavaFile(system_, path);
+      // Note: There is logic in javax.swing.plaf.basic.BasicFileChooserUI#ApproveSelectionAction.actionPerformed() that queries if the file is "absolute".  If not absolute, the logic prepends the directory path.  For that reason, each instance of IFSJavaFile needs to retain an awareness of whether it was created with an absolute path or not.
+      if (isFileSystemRoot(file)) {
+        return createFileSystemRoot(file);
+      }
+      else return file;
+    }
+
+
+    // Note: The method createFileSystemRoot() was added to the FileSystemView class in JDK 1.4.
+    // We provide an implementation here to swallow the NoSuchMethodError if running on an older JDK.
+    protected File createFileSystemRoot(File f) {
+      try {
+        return super.createFileSystemRoot(f);
+      }
+      catch (NoSuchMethodError e) {  // method added in JDK 1.4
+        if (Trace.isTraceOn()) {
+          Trace.log(Trace.DIAGNOSTIC, e);
+        }
+        return f;
+      }
     }
 
     /**
@@ -130,33 +165,58 @@ public class IFSFileSystemView extends FileSystemView
     public File createNewFolder(File containingDir)
     throws IOException
     {
-        try
-        {
-            IFSJavaFile newFolder = new IFSJavaFile((IFSJavaFile)containingDir, NEW_FOLDER_STRING);
-            int i=1;
-            while(newFolder.exists() && (i<100))
-            {
-                newFolder = new IFSJavaFile((IFSJavaFile)containingDir, MessageFormat.format(
-                                                                                            NEW_FOLDER_NEXT_STRING, new Object[] { new Integer(i++)}));
-            }
+      if (containingDir == null) throw new IOException("Containing directory is null:");  // This is what javax.swing.filechooser.FileSystemView does.
 
-            if(newFolder.exists())
-            {
-                throw new IOException("Directory already exists:" + newFolder.getAbsolutePath());
-            }
-            else
-            {
-                newFolder.mkdirs();
-                return newFolder;
-            }
-        }
-        catch(ClassCastException e)
-        {
-            Trace.log(Trace.ERROR, "Argument must be of type IFSJavaFile.");
-            throw new ExtendedIllegalArgumentException("containingDir",
-                                                       ExtendedIllegalArgumentException.PARAMETER_VALUE_NOT_VALID);
-        }
+      IFSJavaFile ifsDir;
+      if (DEBUG) {
+        ifsDir = convertToIFSJavaFile(containingDir, "createNewFolder");
+      }
+      else {
+        ifsDir = convertToIFSJavaFile(containingDir);
+      }
+      IFSJavaFile newFolder = new IFSJavaFile(ifsDir, NEW_FOLDER_STRING);
+      int i=1;
+      while (newFolder.exists() && (i<100))
+      {
+        newFolder = new IFSJavaFile((IFSJavaFile)containingDir,
+                                    MessageFormat.format(
+                                      NEW_FOLDER_NEXT_STRING, new Object[] { new Integer(i++)}));
+      }
+
+      if (newFolder.exists())
+      {
+        throw new IOException("Directory already exists:" + newFolder.getAbsolutePath());
+      }
+      else
+      {
+        newFolder.mkdirs();
+        return newFolder;
+      }
     }
+
+
+    // The parent's implementation works for us.  If not, here's what we'd do:
+    //  public File getChild(File parent,
+    //                 String fileName)
+    //  {
+    //  if (parent == null) throw new NullPointerException("parent");
+    //  if (fileName == null) throw new NullPointerException("fileName");
+    //
+    //  return createFileObject(parent, fileName);
+    //  }
+
+
+    /**
+     * Returns the user's default starting directory for the file chooser.
+     * This will represent the 'root' directory on the server.
+     * @return A <code>File</code> object representing the default
+     *         starting folder.
+     **/
+    public File getDefaultDirectory()
+    {
+      return createFileSystemRoot(new IFSJavaFile(system_, ROOT_PATH));
+    }
+
 
     /**
      Gets the list of shown (that is, not hidden) files in the directory.
@@ -166,7 +226,16 @@ public class IFSFileSystemView extends FileSystemView
      **/
     public File[] getFiles(File directory, boolean useFileHiding)
     {
-        return directory.listFiles();
+      if (directory == null) throw new NullPointerException("directory");
+
+      IFSJavaFile ifsDir;
+      if (DEBUG) {
+        ifsDir = convertToIFSJavaFile(directory, "getFiles");
+      }
+      else {
+        ifsDir = convertToIFSJavaFile(directory);
+      }
+      return ifsDir.listFiles();
     }
 
     /**
@@ -176,19 +245,31 @@ public class IFSFileSystemView extends FileSystemView
      **/
     public File getHomeDirectory()
     {
-        return new IFSJavaFile(system_, ROOT_PATH);
+      return (new IFSJavaFile(system_, ROOT_PATH));
     }
 
     /**
-     Returns the parent directory of specified directory. 
-     @param directory The directory being queried.
-     <br>directory must be of type {@link com.ibm.as400.access.IFSJavaFile IFSJavaFile}.
-     @return the parent directory of specified directory, or null if specified directory is null.
+     Returns the parent directory of <tt>dir</tt>. 
+     @param dir The directory being queried.
+     <br><tt>dir</tt> must be of type {@link com.ibm.as400.access.IFSJavaFile IFSJavaFile}.
+     @return the parent directory of <tt>dir</tt>, or null if <tt>dir</tt> is null.
      **/
-    public File getParentDirectory(File directory)
+    public File getParentDirectory(File dir)
     {
-        if(directory == null) return null;
-        else return directory.getParentFile();
+      if (dir == null) return null;
+
+      IFSJavaFile ifsDir;
+      if (DEBUG) {
+        ifsDir = convertToIFSJavaFile(dir, "getParentDirectory");
+      }
+      else {
+        ifsDir = convertToIFSJavaFile(dir);
+      }
+      IFSJavaFile parent = (IFSJavaFile)ifsDir.getParentFile();
+      if (isFileSystemRoot(parent)) {
+        return createFileSystemRoot(parent);
+      }
+      else return parent;
     }
 
     /**
@@ -198,18 +279,108 @@ public class IFSFileSystemView extends FileSystemView
      **/
     public File[] getRoots()
     {
-        return new IFSJavaFile[] { new IFSJavaFile(system_, ROOT_PATH)};
+      return new File[] { createFileSystemRoot(new IFSJavaFile(system_, ROOT_PATH)) };
     }
 
     /**
-     Returns whether a file is hidden or not. 
-     @param file The file.
-     @return true if the file is hidden.
+     * Returns the name of a file, directory, or folder as it would be displayed in
+     * a system file browser.
+     *
+     * @param f A <code>File</code> object.
+     * @return The file name as it would be displayed by a native file chooser.
      **/
-    public boolean isHiddenFile(File file)
+    public String getSystemDisplayName(File f)
     {
-        return file.isHidden();
+      if (f == null) return null;
+
+      return f.getName();
     }
+
+    /**
+     * Always returns an icon representing an iSeries server.
+     **/
+    public Icon getSystemIcon(File f)
+    {
+      return serverIcon32_;
+    }
+
+    /**
+     * Returns a type description for a file, directory, or folder as it would be displayed in
+     * a system file browser.
+     * @param f A <code>File</code> object.
+     * @return The file type description as it would be displayed by a native file chooser
+     * or null if no native information is available.
+     **/
+    public String getSystemTypeDescription(File f)
+    {
+      return system_.getSystemName();
+    }
+
+    /**
+     Always returns false.
+     **/
+    public boolean isComputerNode(File dir)
+    {
+      return false;  // no "computer nodes" in IFS
+    }
+
+    /**
+     Always returns false.
+     **/
+    public boolean isDrive(File dir)
+    {
+      return false;  // no "drives" in IFS
+    }
+
+    /**
+     Always returns false.
+     **/
+    public boolean isFloppyDrive(File dir)
+    {
+      return false;  // no "floppy drives" in IFS
+    }
+
+    // The parent's implementation works for us.  If not, here's what we'd do:
+    //  public boolean isFileSystem(File file)
+    //  {
+    //    if (file == null) throw new NullPointerException("file");
+    //
+    //    return (!file.getPath().startsWith("ShellFolder"));
+    //  }
+
+
+    /**
+     * Returns true if f represents the root directory on the server ("/"),
+     * and false otherwise.
+     *
+     * @param f A <code>File</code> object representing a directory.
+     * @return <code>true</code> if <code>f</code> is a root of a filesystem
+     **/
+    public boolean isFileSystemRoot(File f)
+    {
+      if (f == null) return false;
+
+      return (f.getAbsolutePath().equals(ROOT_PATH));
+    }
+
+
+    // The parent's implementation works for us.  If not, here's what we'd do:
+    //  public boolean isHiddenFile(File file)
+    //  {
+    //    if (file == null) throw new NullPointerException("file");
+    //
+    //    return file.isHidden();
+    //  }
+
+
+    // The parent's implementation works for us.  If not, here's what we'd do:
+    //  public boolean isParent(File folder, File file)
+    //  {
+    //    if (folder == null || file == null) return false;
+    //
+    //    return folder.equals(file.getParentFile());
+    //  }
+
 
     /**
      Determines if the given file is a root in the navigatable tree(s).
@@ -219,8 +390,57 @@ public class IFSFileSystemView extends FileSystemView
      **/
     public boolean isRoot(File file)
     {
-        if(file.getAbsolutePath().equals(ROOT_PATH)) return true;
-        else return false;
+      // Note: Normally we'd just use the parent's method, but older JDK's (pre-1.4) seem to have difficulty unless we provide our own.
+      if (file == null || !file.isAbsolute()) {
+        return false;
+      }
+
+      return (file.getAbsolutePath().equals(ROOT_PATH));
+    }
+
+
+    // The parent's implementation works for us.  If not, here's what we'd do:
+    //  public Boolean isTraversable(File file)
+    //  {
+    //    if (file == null) throw new NullPointerException("file");
+    //
+    //    return Boolean.valueOf(file.isDirectory());
+    //
+    //  }
+
+
+    // Utility method.
+    private IFSJavaFile convertToIFSJavaFile(File file)
+    {
+      if (file instanceof IFSJavaFile) {
+        return (IFSJavaFile)file;
+      }
+
+      if (Trace.isTraceOn()) {
+        Trace.log(Trace.DIAGNOSTIC,
+                  "File is not an IFSJavaFile.  File is of type " + file.getClass().getName());
+      }
+      // Create an IFSJavaFile using the path contained in the File object.
+      return new IFSJavaFile(system_, file.getPath());
+    }
+
+
+    // Utility method.  Additionally accepts a methodName, for more-descriptive tracing.
+    private IFSJavaFile convertToIFSJavaFile(File file, String methodName)
+    {
+      if (DEBUG) {
+        if (file instanceof IFSJavaFile) {
+          return (IFSJavaFile)file;
+        }
+
+        if (Trace.isTraceOn()) {
+          Trace.log(Trace.DIAGNOSTIC,
+                    "File is not an IFSJavaFile.  File is of type " + file.getClass().getName() + ". Caller was " + methodName + ".");
+        }
+        // Create an IFSJavaFile using the path contained in the File object.
+        return new IFSJavaFile(system_, file.getPath());
+      }
+      else return convertToIFSJavaFile(file);
     }
 
 }
