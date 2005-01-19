decec2a86a00b2c8705cb7530ac7fa0a2b65ab9c
wrightj-oss <wrightj-oss>
2005-01-19 19:57:16 +0000

    Add ability to authenticate to DDM server with a profile token.

--- src/com/ibm/as400/access/DDMACCSECRequestDataStream.java
+++ src/com/ibm/as400/access/DDMACCSECRequestDataStream.java
@@ -22,7 +22,7 @@ class DDMACCSECRequestDataStream extends DDMDataStream
 
     DDMACCSECRequestDataStream(boolean useStrongEncryption, int byteType, byte[] iasp)
     {
-        super(byteType == AS400.AUTHENTICATION_SCHEME_GSS_TOKEN ? new byte[iasp == null ? 16 : 38] : new byte[iasp == null ? 28 : 50]);
+        super(new byte[byteType == AS400.AUTHENTICATION_SCHEME_PASSWORD ? 28 : 16]);
 
         // Initialize the header:  Don't continue on error, not chained, GDS id = D0, type = RQSDSS, no same request correlation.
         setGDSId((byte)0xD0);
@@ -30,83 +30,61 @@ class DDMACCSECRequestDataStream extends DDMDataStream
         // setContinueOnError(false);
         // setHasSameRequestCorrelation(false);
         setType(1);
-        boolean useRDB = (iasp != null);
-        if (byteType == AS400.AUTHENTICATION_SCHEME_GSS_TOKEN)
+        // Set total length remaining after header.
+        if (byteType == AS400.AUTHENTICATION_SCHEME_PASSWORD)
         {
-            set16bit(useRDB ? 32 : 10, 6); // Set total length remaining after header.
+            set16bit(22, 6);
         }
         else
         {
-            set16bit(useRDB ? 44 : 22, 6); // Set total length remaining after header.
+            set16bit(10, 6);
         }
 
         set16bit(DDMTerm.ACCSEC, 8); // Set ACCSEC code point.
         set16bit(6, 10); // Set SECMEC length.
         set16bit(DDMTerm.SECMEC, 12); // Set SECMEC code point.
-        if (byteType == AS400.AUTHENTICATION_SCHEME_GSS_TOKEN)
+        // Set value of SECMEC parm.
+        switch (byteType)
         {
-            set16bit(11, 14); // Set value of SECMEC parm.
-            if (useRDB)
-            {
-                set16bit(DDMTerm.RDBNAM, 16);
-                set16bit(iasp.length, 18); // Assuming IASP name can only be single-byte EBCDIC
-                System.arraycopy(iasp, 0, data_, 20, iasp.length); 
-            }
-        }
-        else
-        {
-            if (useStrongEncryption)
-            {
-                // Use a value of SECMEC=8 for encrypted password.
-                set16bit(8, 14); // Set value of SECMEC parm.
-            }
-            else
-            {
-                // Use a value of SECMEC=6 for a substituted password.
-                set16bit(6, 14); // Set value of SECMEC parm.
-            }
+            case AS400.AUTHENTICATION_SCHEME_GSS_TOKEN:
+                set16bit(11, 14);
+                break;
+            case AS400.AUTHENTICATION_SCHEME_PROFILE_TOKEN:
+                set16bit(16, 14);
+                break;
+            default:
+                if (useStrongEncryption)
+                {
+                    // Use a value of SECMEC=8 for encrypted password.
+                    set16bit(8, 14);
+                }
+                else
+                {
+                    // Use a value of SECMEC=6 for a substituted password.
+                    set16bit(6, 14);
+                }
 
-            if (useRDB)
-            {
-              set16bit(22, 16); // length
-              set16bit(DDMTerm.RDBNAM, 18);
-              System.arraycopy(iasp, 0, data_, 20, iasp.length);
-            }
+                // Need to send a client seed as the security token.
+                // The SECTKN is 8 bytes and the PASSWORD is 20 bytes.
+                set16bit(12, 16); // Set length of this+remaining SECTKN bytes.
+                set16bit(DDMTerm.SECTKN, 18); // Set SECTKN code point.
 
+                // This code taken from AS400XChgRandSeedDS constructor.  Generate the client seed.  We generate a "random" seed using the current time in milliseconds.  This seed will be used to encrypt the password.
+                long t = System.currentTimeMillis();
 
-            // Need to send a client seed as the security token.
-            // The SECTKN is 8 bytes and the PASSWORD is 20 bytes.
-            set16bit(12, useRDB ? 38 : 16); // Set length of this+remaining SECTKN bytes.
-            set16bit(DDMTerm.SECTKN, useRDB ? 40 : 18); // Set SECTKN code point.
+                // Performance: break into 2 ints first and avoid long temporaries.
+                int high = (int)(t >>> 32);
+                int low = (int)t;
 
-            // This code taken from AS400XChgRandSeedDS constructor.  Generate the client seed.  We generate a "random" seed using the current time in milliseconds.  This seed will be used to encrypt the password.
-            long t = System.currentTimeMillis();
+                data_[20] = (byte)(high >>> 24);
+                data_[21] = (byte)(high >>> 16);
+                data_[22] = (byte)(high >>> 8);
+                data_[23] = (byte)high;
 
-            // Performance: break into 2 ints first and avoid long temporaries.
-            int high = (int)(t >>> 32);
-            int low = (int)t;
-
-            int offset = useRDB ? 42 : 20;
-            data_[offset] = (byte)(high >>> 24);
-            data_[offset+1] = (byte)(high >>> 16);
-            data_[offset+2] = (byte)(high >>> 8);
-            data_[offset+3] = (byte)high;
-
-            data_[offset+4] = (byte)(low >>> 24);
-            data_[offset+5] = (byte)(low >>> 16);
-            data_[offset+6] = (byte)(low >>> 8);
-            data_[offset+7] = (byte)low;
-/*
-            data_[20] = (byte)(high >>> 24);
-            data_[21] = (byte)(high >>> 16);
-            data_[22] = (byte)(high >>> 8);
-            data_[23] = (byte)high;
-
-            data_[24] = (byte)(low >>> 24);
-            data_[25] = (byte)(low >>> 16);
-            data_[26] = (byte)(low >>> 8);
-            data_[27] = (byte)low;
-*/
+                data_[24] = (byte)(low >>> 24);
+                data_[25] = (byte)(low >>> 16);
+                data_[26] = (byte)(low >>> 8);
+                data_[27] = (byte)low;
         }
     }
 
--- src/com/ibm/as400/access/DDMSECCHKRequestDataStream.java
+++ src/com/ibm/as400/access/DDMSECCHKRequestDataStream.java
@@ -22,12 +22,11 @@ class DDMSECCHKRequestDataStream extends DDMDataStream
 
     DDMSECCHKRequestDataStream(byte[] userIDbytes, byte[] authenticationBytes, byte[] iasp, int byteType)
     {
-        super(byteType == AS400.AUTHENTICATION_SCHEME_GSS_TOKEN ? new byte[authenticationBytes.length + (iasp == null ? 26 : 48)] : new byte[authenticationBytes.length + (iasp == null ? 34 : 56)]);
+        super(new byte[authenticationBytes.length + (iasp == null ? 0 : 22) + (byteType == AS400.AUTHENTICATION_SCHEME_PASSWORD ? 34 : 26)]);
 
         // Initialize the header:  Don't continue on error, not chained, GDS id = D0, type = RQSDSS, no same request correlation.
         setGDSId((byte)0xD0);
-        boolean useRDB = (iasp != null);
-        if (byteType == AS400.AUTHENTICATION_SCHEME_GSS_TOKEN)
+        if (byteType != AS400.AUTHENTICATION_SCHEME_PASSWORD)
         {
             setLength(16);
             setIsChained(true);
@@ -38,8 +37,15 @@ class DDMSECCHKRequestDataStream extends DDMDataStream
             set16bit(DDMTerm.SECCHK, 8);  // Set code point for SECCHK.
             set16bit(6, 10);  // Set LL for SECMEC term.
             set16bit(DDMTerm.SECMEC, 12); // Set code point for SECMEC.
-            set16bit(11, 14); // Set value of SECMEC parm.
-
+            // Set value of SECMEC parm.
+            if (byteType == AS400.AUTHENTICATION_SCHEME_GSS_TOKEN)
+            {
+                set16bit(11, 14);
+            }
+            else
+            {
+                set16bit(16, 14);
+            }
             set16bit(authenticationBytes.length + 10, 16);  // Set LL for SECCHK term.
             set16bit(0xD003, 18);
 
@@ -49,17 +55,15 @@ class DDMSECCHKRequestDataStream extends DDMDataStream
         }
         else
         {
+            boolean useRDB = (iasp != null);
             // setIsChained(false);
             // setContinueOnError(false);
             // setHasSameRequestCorrelation(false);
             setType(1);
-
             set16bit(authenticationBytes.length + (useRDB ? 50 : 28), 6);  // Set LL for SECCHK term.
             set16bit(DDMTerm.SECCHK, 8);  // Set code point for SECCHK.
-
             set16bit(6, 10);  // Set LL for SECMEC term.
             set16bit(DDMTerm.SECMEC, 12); // Set code point for SECMEC.
-
             if (authenticationBytes.length == 20)
             {
                 set16bit(8, 14); // Set value for SECMEC term.
@@ -68,7 +72,6 @@ class DDMSECCHKRequestDataStream extends DDMDataStream
             {
                 set16bit(6, 14);  // Set value for SECMEC term.
             }
-
             set16bit(14, 16);  // Set LL for USRID term.
             set16bit(DDMTerm.USRID, 18);  // Set code point for USRID.
             // Set the user ID.
@@ -78,13 +81,14 @@ class DDMSECCHKRequestDataStream extends DDMDataStream
             set16bit(DDMTerm.PASSWORD, 32);  // Set code point for PASSWORD.
             // Set the password.
             System.arraycopy(authenticationBytes, 0, data_, 34, authenticationBytes.length);
+
             if (useRDB)
             {
-              if (Trace.traceOn_) Trace.log(Trace.DIAGNOSTIC, "Sending SECCHK request with RDB / IASP name.");
-              int offset = 34+authenticationBytes.length;
-              set16bit(22, offset);
-              set16bit(DDMTerm.RDBNAM, offset+2);
-              System.arraycopy(iasp, 0, data_, offset+4, iasp.length);
+                if (Trace.traceOn_) Trace.log(Trace.DIAGNOSTIC, "Sending SECCHK request with RDB / IASP name.");
+                int offset = 34 + authenticationBytes.length;
+                set16bit(22, offset);
+                set16bit(DDMTerm.RDBNAM, offset + 2);
+                System.arraycopy(iasp, 0, data_, offset + 4, iasp.length);
             }
         }
     }
