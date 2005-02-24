3176122597183bbbd1cc9380db418adc27e8076c
jlee-oss <jlee-oss>
2005-02-24 18:24:22 +0000

    Re-added static copyright string for now.

--- src/com/ibm/as400/access/Copyright.java
+++ src/com/ibm/as400/access/Copyright.java
@@ -18,5 +18,6 @@ package com.ibm.as400.access;
  **/
 public interface Copyright
 {
+    public static final String copyright = "Copyright (C) 1997-2005 International Business Machines Corporation and others.";
     public static final String version   = "Open Source Software, JTOpen 4.7, codebase 5722-JC1 V5R3M0.7";
 }
