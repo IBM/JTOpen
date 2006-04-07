6b15e8d5b57ca3557b4d8b4efb5e7701c2ec2fd1
wrightj-oss <wrightj-oss>
2006-04-07 18:35:58 +0000

    Avoid StringIndexOutOfBoundsException in trace.

--- src/com/ibm/as400/access/AS400.java
+++ src/com/ibm/as400/access/AS400.java
@@ -1961,8 +1961,8 @@ public class AS400 implements Serializable
                     String loadPath = loadUrl.getPath();
                     Trace.log(Trace.DIAGNOSTIC, "Path of AS400 class: " + thisPath);
                     Trace.log(Trace.DIAGNOSTIC, "Path of loaded impl class: " + loadPath);
-                    String thisDirPath = thisPath.length() <= thisFileName.length() ? "/" : thisPath.substring(0, thisPath.length() - thisFileName.length() - 1);
-                    String loadDirPath = loadPath.length() <= loadFileName.length() ? "/" : loadPath.substring(0, loadPath.length() - loadFileName.length() - 1);
+                    String thisDirPath = thisPath.length() <= thisFileName.length() ? "" : thisPath.substring(0, thisPath.length() - thisFileName.length() - 1);
+                    String loadDirPath = loadPath.length() <= loadFileName.length() ? "" : loadPath.substring(0, loadPath.length() - loadFileName.length() - 1);
                     if (!thisDirPath.equals(loadDirPath))
                     {
                         Trace.log(Trace.WARNING, "Toolbox classes found at " + thisDirPath + " and " + loadDirPath);
