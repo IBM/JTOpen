2df7808f9db5ba97ae93138be14524a9a4055faa
pauldevman <pauldevman>
2007-05-09 19:40:09 +0000

    Performance enhancments on native Pase environment.

--- src/com/ibm/as400/access/NativeMethods.java
+++ src/com/ibm/as400/access/NativeMethods.java
@@ -18,7 +18,33 @@ public class NativeMethods
 {
     static
     {
-        System.load("/QSYS.LIB/QYJSPART.SRVPGM");
+        // Check to see which version of native code to use                 //@pase1
+        if (System.getProperty("java.vm.name").indexOf("Classic VM") < 0)   //@pase1
+        {                                                                   //@pase1
+            try{                                                            //@pase1
+                //we are in j9 jvm
+                if (System.getProperty("java.home").indexOf("64bit") > 0)       //@pase1
+                {                                                               //@pase1
+                    //load 64 bit version
+                    if (Trace.traceOn_) Trace.log(Trace.DIAGNOSTIC, "Loading Native PASE methods for 64bit libs if available");//@pase1
+                    System.load("/QIBM/ProdData/OS400/jt400/lib/qyjspase64.so");  //@pase1
+
+                } else                                                          //@pase1
+                {                                                               //@pase1
+                    //load 32 bit version
+                    if (Trace.traceOn_) Trace.log(Trace.DIAGNOSTIC, "Loading Native PASE methods for 32bit libs if available");//@pase1
+                    System.load("/QIBM/ProdData/OS400/jt400/lib/qyjspase32.so");   
+                }                                                               //@pase1
+            }catch(Throwable t)                                                 //@pase1
+            {                                                                   //@pase1
+                //note that if pase version load of libs fail (ie do not exist), then then we just default to ile version below
+                if (Trace.traceOn_) Trace.log(Trace.ERROR, t);                  //@pase1
+            }                                                                   //@pase1
+
+        }    
+        
+        if (Trace.traceOn_) Trace.log(Trace.DIAGNOSTIC, "Loading Native non-PASE methods ");  //@pase1
+        System.load("/QSYS.LIB/QYJSPART.SRVPGM");  //if j9, then socket functions in this lib are overridden        
     }
 
     static void load()
