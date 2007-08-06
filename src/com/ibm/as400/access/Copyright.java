138660cf06a436dafbb816084adc0ef34320684a
coover-oss <coover-oss>
2007-08-06 13:32:43 +0000

    Updated for JTOpen 6.0

--- src/com/ibm/as400/access/Copyright.java
+++ src/com/ibm/as400/access/Copyright.java
@@ -20,5 +20,5 @@ public interface Copyright
 {
     /** @deprecated This field is reserved for use within the Toolbox product. **/
     public static final String copyright = "Copyright (C) 1997-2007 International Business Machines Corporation and others.";
-    public static final String version   = "Open Source Software, JTOpen 5.4, codebase 5722-JC1 V5R4M0.4";
+    public static final String version   = "Open Source Software, JTOpen 6.0, codebase 5722-JC1 V5R4M0.6";
 }
