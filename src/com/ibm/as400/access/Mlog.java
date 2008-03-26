e040224ccb20627ee0419673241fce906abf3601
jlee-oss <jlee-oss>
2008-03-26 17:34:13 +0000

    Made the Mlog class non-public.

--- src/com/ibm/as400/access/Mlog.java
+++ src/com/ibm/as400/access/Mlog.java
@@ -30,7 +30,7 @@ import java.nio.CharBuffer;
  * This class is used for tracing purposes
  *
  */
-public class Mlog
+class Mlog
 {
     final static char[] hexa = new char[] {'0','1','2','3','4','5','6','7',
                                            '8','9','a','b','c','d','e','f'};
