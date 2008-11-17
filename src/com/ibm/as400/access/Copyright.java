b71a940084cf64d19a736747bcf495b919d7d15f
jlee-oss <jlee-oss>
2008-11-17 21:42:37 +0000

    Added constants for use by AS400JDBCDriver.

--- src/com/ibm/as400/access/Copyright.java
+++ src/com/ibm/as400/access/Copyright.java
@@ -21,4 +21,9 @@ public interface Copyright
     /** @deprecated This field is reserved for use within the Toolbox product. **/
     public static final String copyright = "Copyright (C) 1997-2008 International Business Machines Corporation and others.";
     public static final String version   = "Open Source Software, JTOpen 6.4, codebase 5761-JC1 V6R1M0.6";
+
+    // Constants for reference by AS400JDBCDriver.
+    static final int    MAJOR_VERSION = 8;  // ex: "8" indicates V6R1
+    static final int    MINOR_VERSION = 6;  // ex: "6" indicates PTF #6
+    static final String DRIVER_LEVEL  = "06010006"; //(ex: 05040102->V5R4M1.2) (needed for hidden clientInfo) (each # is 2 digits in length)
 }
