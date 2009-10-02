8a31cfe2ef21a8b3e4a0fc277ac808a9de9013d1
jlee-oss <jlee-oss>
2009-10-02 15:20:31 +0000

    Added new method: Trace.toHexString(byte).

--- src/com/ibm/as400/access/Trace.java
+++ src/com/ibm/as400/access/Trace.java
@@ -1136,18 +1136,12 @@ public class Trace
 
     for (int i = 0; i < length; i++, offset++)
     {
-      int leftDigitValue = (data[offset] >>> 4) & 0x0F;
-      int rightDigitValue = data[offset] & 0x0F;
-      // 0x30 = '0', 0x41 = 'A'
-      char leftDigit = leftDigitValue < 0x0A ? (char)(0x30 + leftDigitValue) : (char)(leftDigitValue - 0x0A + 0x41);
-      char rightDigit = rightDigitValue < 0x0A ? (char)(0x30 + rightDigitValue) : (char)(rightDigitValue - 0x0A + 0x41);
-      pw.print(leftDigit);
-      pw.print(rightDigit);
+      pw.print(toHexString(data[offset]));
       pw.print(" ");
 
       if ((i & 0x0F ) == 0x0F)
       {
-        pw.println();
+        pw.println();  // start a new line
       }
     }
     if (((length - 1) & 0x0F) != 0x0F)
@@ -1179,18 +1173,12 @@ public class Trace
 
     for (int i = 0; i < length; i++, offset++)
     {
-      int leftDigitValue = (data[offset] >>> 4) & 0x0F;
-      int rightDigitValue = data[offset] & 0x0F;
-      // 0x30 = '0', 0x41 = 'A'
-      char leftDigit = leftDigitValue < 0x0A ? (char)(0x30 + leftDigitValue) : (char)(leftDigitValue - 0x0A + 0x41);
-      char rightDigit = rightDigitValue < 0x0A ? (char)(0x30 + rightDigitValue) : (char)(rightDigitValue - 0x0A + 0x41);
-      buf.append(leftDigit);
-      buf.append(rightDigit);
+      buf.append(toHexString(data[offset]));
       buf.append(" ");
 
       if ((i & 0x0F ) == 0x0F)
       {
-        buf.append("\n");
+        buf.append("\n");  // start a new line
       }
     }
     if (((length - 1) & 0x0F) != 0x0F)
@@ -1198,6 +1186,38 @@ public class Trace
       // Finish the line of data.
       buf.append("\n");
     }
+  }
+
+  // Hexadecimal string representations of all possible values for a single byte.
+  private static final String[] HEX_BYTE_ARRAY = new String[]
+  {
+    "00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "0A", "0B", "0C", "0D", "0E", "0F", 
+    "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "1A", "1B", "1C", "1D", "1E", "1F", 
+    "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "2A", "2B", "2C", "2D", "2E", "2F", 
+    "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "3A", "3B", "3C", "3D", "3E", "3F", 
+    "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "4A", "4B", "4C", "4D", "4E", "4F", 
+    "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "5A", "5B", "5C", "5D", "5E", "5F", 
+    "60", "61", "62", "63", "64", "65", "66", "67", "68", "69", "6A", "6B", "6C", "6D", "6E", "6F", 
+    "70", "71", "72", "73", "74", "75", "76", "77", "78", "79", "7A", "7B", "7C", "7D", "7E", "7F", 
+    "80", "81", "82", "83", "84", "85", "86", "87", "88", "89", "8A", "8B", "8C", "8D", "8E", "8F", 
+    "90", "91", "92", "93", "94", "95", "96", "97", "98", "99", "9A", "9B", "9C", "9D", "9E", "9F", 
+    "A0", "A1", "A2", "A3", "A4", "A5", "A6", "A7", "A8", "A9", "AA", "AB", "AC", "AD", "AE", "AF", 
+    "B0", "B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9", "BA", "BB", "BC", "BD", "BE", "BF", 
+    "C0", "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "CA", "CB", "CC", "CD", "CE", "CF", 
+    "D0", "D1", "D2", "D3", "D4", "D5", "D6", "D7", "D8", "D9", "DA", "DB", "DC", "DD", "DE", "DF", 
+    "E0", "E1", "E2", "E3", "E4", "E5", "E6", "E7", "E8", "E9", "EA", "EB", "EC", "ED", "EE", "EF", 
+    "F0", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "FA", "FB", "FC", "FD", "FE", "FF"
+  };
+
+
+  /**
+   Returns a string representation of the byte argument, as 2 hexadecimal digits.
+   @param b A byte to be converted to a string.
+   @return The 2-digit hexadecimal string representation of the byte argument.
+   **/
+  public static final String toHexString(byte b)
+  {
+    return HEX_BYTE_ARRAY[0x00FF & b];
   }
 
 
