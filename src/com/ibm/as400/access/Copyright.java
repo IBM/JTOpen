3e0b03e915270a0e3b0948dfc4815fccb69c92de
jlee-oss <jlee-oss>
2010-01-19 18:24:40 +0000

    Updated for JTOpen 6.7

--- src/com/ibm/as400/access/Copyright.java
+++ src/com/ibm/as400/access/Copyright.java
@@ -19,11 +19,11 @@ package com.ibm.as400.access;
 public interface Copyright
 {
     /** @deprecated This field is reserved for use within the Toolbox product. **/
-    public static final String copyright = "Copyright (C) 1997-2009 International Business Machines Corporation and others.";
-    public static final String version   = "Open Source Software, JTOpen 6.6, codebase 5761-JC1 V6R1M0.10";
+    public static final String copyright = "Copyright (C) 1997-2010 International Business Machines Corporation and others.";
+    public static final String version   = "Open Source Software, JTOpen 6.7, codebase 5761-JC1 V6R1M0.11";
 
     // Constants for reference by AS400JDBCDriver.
     static final int    MAJOR_VERSION = 8;  // ex: "8" indicates V6R1
-    static final int    MINOR_VERSION = 10; // ex: "8" indicates PTF #8 //Note: JTOpen 6.5 is synching with ptf8
-    static final String DRIVER_LEVEL  = "06010010"; //(ex: 05040102 -> V5R4M1 PTF#2) (needed for hidden clientInfo) (each # is 2 digits in length)
+    static final int    MINOR_VERSION = 11; // ex: "8" indicates PTF #8 //Note: JTOpen 6.5 is synching with ptf8
+    static final String DRIVER_LEVEL  = "06010011"; //(ex: 05040102 -> V5R4M1 PTF#2) (needed for hidden clientInfo) (each # is 2 digits in length)
 }
