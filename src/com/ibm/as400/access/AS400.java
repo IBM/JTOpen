fefe1cbf36602608befee7ba393389e433331063
jlee-oss <jlee-oss>
2010-02-22 18:48:30 +0000

    Eliminated limitation on getJobs() method, regarding the RECORDACCESS service.

--- src/com/ibm/as400/access/AS400.java
+++ src/com/ibm/as400/access/AS400.java
@@ -1490,7 +1490,6 @@ public class AS400 implements Serializable
      <li>{@link #SIGNON SIGNON} - sign-on classes.
      </ul>
      @return  The array of job objects.
-     <br>Note: Due to current design limitations, if {@link #RECORDACCESS RECORDACCESS} is specified, a zero-length list is returned.
      **/
     public Job[] getJobs(int service)
     {
