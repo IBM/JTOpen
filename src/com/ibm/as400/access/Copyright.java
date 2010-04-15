a459880697cc5139ca22ad01ef7b8a97e3422681
John Eberhard <jeber@us.ibm.com>
2010-04-15 19:48:54 +0000

    JTOpen Release 7.0

--- src/com/ibm/as400/access/Copyright.java
+++ src/com/ibm/as400/access/Copyright.java
@@ -20,10 +20,11 @@ public interface Copyright
 {
     /** @deprecated This field is reserved for use within the Toolbox product. **/
     public static final String copyright = "Copyright (C) 1997-2010 International Business Machines Corporation and others.";
-    public static final String version   = "Open Source Software, JTOpen 6.7, codebase 5761-JC1 V6R1M0.11";
+    public static final String version   = "Open Source Software, JTOpen 7.0, codebase 5770-JC1 V7R1M0.01";
 
     // Constants for reference by AS400JDBCDriver.
-    static final int    MAJOR_VERSION = 8;  // ex: "8" indicates V6R1
-    static final int    MINOR_VERSION = 11; // ex: "8" indicates PTF #8 //Note: JTOpen 6.5 is synching with ptf8
+    static final int    MAJOR_VERSION = 9;  // ex: "9" indicates V7R1
+    static final int    MINOR_VERSION = 1; // ex: "1" indicates PTF #1 //Note: JTOpen 7.0 is synching with ptf 9.1
     static final String DRIVER_LEVEL  = "07010001"; //(ex: 05040102 -> V5R4M1 PTF#2) (needed for hidden clientInfo) (each # is 2 digits in length)
+    
 }
