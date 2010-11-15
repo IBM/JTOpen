94f1ddcfe6eaab258cd7cb484e2446cb36dbef07
John Eberhard <jeber@us.ibm.com>
2010-11-15 20:53:22 +0000

    Add stub methods to alloc JDBC micro to compile with JD 1.4 java.sql

--- micro/java/sql/Date.java
+++ micro/java/sql/Date.java
@@ -1,5 +1,8 @@
 package java.sql;
 
 public class Date  extends java.util.Date{
+    public Date(long milliseconds) {
+   	 super(milliseconds); 
+    }
 
 }
--- micro/java/sql/Time.java
+++ micro/java/sql/Time.java
@@ -1,5 +1,7 @@
 package java.sql;
 
 public class Time  extends java.util.Date {
-
+     public Time(long milliseconds) {
+    	 super(milliseconds); 
+     }
 }
