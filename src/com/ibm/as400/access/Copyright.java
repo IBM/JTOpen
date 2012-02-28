52a06d26844b7d18dcfe499b0c2a86b039089310
John Eberhard <jeber@us.ibm.com>
2012-02-28 17:29:23 +0000

    Fix java.util.NoSuchElementException: Vector Enumeration thrown by rollback

--- src/com/ibm/as400/access/Copyright.java
+++ src/com/ibm/as400/access/Copyright.java
@@ -85,7 +85,7 @@ public interface Copyright
 {
     /** @deprecated  This field is reserved for use within the Toolbox product. **/
     public static String copyright = "Copyright (C) 1997-2011 International Business Machines Corporation and others.";
-    public static String version   = "Open Source Software, JTOpen 7.6, codebase 5770-SS1 V7R1M0.06 2012/02/13 @F9";  // As of V7R1: JC1 is merged into SS1 Option 3
+    public static String version   = "Open Source Software, JTOpen 7.6, codebase 5770-SS1 V7R1M0.06 2012/02/28 @FA";  // As of V7R1: JC1 is merged into SS1 Option 3
 
     // Constants for reference by AS400JDBCDriver.
     static final int    MAJOR_VERSION = 9; // ex: "9" indicates V7R1
