301ee540abb391efd0e28506bf49822e3276d607
John Eberhard <jeber@us.ibm.com>
2012-05-08 20:27:35 +0000

    JTOpenLite Beta

---
+++ jtopenlite/com/ibm/jtopenlite/About.java
@@ -0,0 +1,19 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// JTOpenLite
+//
+// Filename:  About.java
+//
+// The source code contained herein is licensed under the IBM Public License
+// Version 1.0, which has been approved by the Open Source Initiative.
+// Copyright (C) 2011-2012 International Business Machines Corporation and
+// others.  All rights reserved.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+package com.ibm.jtopenlite;
+
+public class About {
+    public static String INTERFACE_NAME="jtopenlite";
+    public static String INTERFACE_LEVEL="20120508";            // We just use the data as the current interface level
+}
---
+++ jtopenlite/com/ibm/jtopenlite/Trace.java
@@ -0,0 +1,87 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// JTOpenLite
+//
+// Filename:  Trace.java
+//
+// The source code contained herein is licensed under the IBM Public License
+// Version 1.0, which has been approved by the Open Source Initiative.
+// Copyright (C) 2011-2012 International Business Machines Corporation and
+// others.  All rights reserved.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+package com.ibm.jtopenlite;
+
+import java.io.File;
+import java.io.PrintStream;
+
+/**
+ * Class representing the Tracing provided by jtopenlite.
+ *
+ */
+public class Trace {
+
+  public final static int CATEGORY_NONE       = 0x00;
+  public final static int CATEGORY_DATASTREAM = 0x01;
+  public final static int CATEGORY_ALL        = 0xFF;
+
+  static int traceCategory_ = CATEGORY_NONE;
+
+  static PrintStream printStream = System.out;
+
+  static {
+      String category = System.getProperty("com.ibm.jtopenlite.Trace.category");
+
+      String filename = System.getProperty("com.ibm.jtopenlite.Trace.file");
+      if (filename != null) {
+        File file = new File(filename);
+        setPrintFile(file);
+      }
+
+      if (category != null) {
+        category = category.toUpperCase().trim();
+        if (category.equals("TRUE") || category.equals("ALL") ) {
+          traceCategory_ = CATEGORY_ALL;
+          if (printStream == System.out) {
+            printStream.println("Tracing "+About.INTERFACE_NAME+" : level "+About.INTERFACE_LEVEL);
+          }
+        }
+      }
+
+  }
+
+
+
+  public static void setTraceCategory(int traceCategory) {
+    traceCategory_ = traceCategory;
+  }
+
+  public static int getTraceCategory() {
+    return traceCategory_;
+  }
+
+  public static void setPrintFile(File file) {
+     try {
+       printStream = new PrintStream(file);
+       printStream.println("Tracing "+About.INTERFACE_NAME+" : level "+About.INTERFACE_LEVEL);
+     } catch (Exception e) {
+       dumpException(e);
+     }
+  }
+
+  public static PrintStream getPrintStream() {
+    return printStream;
+  }
+
+  public static boolean isStreamTracingEnabled() {
+    return (traceCategory_ & CATEGORY_DATASTREAM) != 0 ;
+  }
+
+  public static void dumpException(Exception e) {
+    if (printStream != null) {
+       e.printStackTrace(printStream);
+    }
+  }
+
+}
