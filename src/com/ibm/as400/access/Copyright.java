aa8b89727ab10554cd7962712ab30a3d769099c5
John Eberhard <jeber@us.ibm.com>
2012-05-10 19:07:29 +0000

    JTOpenLite Beta

--- src/com/ibm/as400/access/Copyright.java
+++ src/com/ibm/as400/access/Copyright.java
@@ -86,7 +86,8 @@ public interface Copyright
 {
     /** @deprecated  This field is reserved for use within the Toolbox product. **/
     public static String copyright = "Copyright (C) 1997-2011 International Business Machines Corporation and others.";
-    public static String version   = "Open Source Software, JTOpen 7.7, codebase 5770-SS1 V7R1M0.07 2012/02/28 @FA";  // As of V7R1: JC1 is merged into SS1 Option 3
+    //                                                                                              built=XXXXXXX is automatically set by the build (see build.xml)
+    public static String version   = "Open Source Software, JTOpen 7.7, codebase 5770-SS1 V7R1M0.07 built=20120228 @FB";  // As of V7R1: JC1 is merged into SS1 Option 3
 
     // Constants for reference by AS400JDBCDriver.
     static final int    MAJOR_VERSION = 9; // ex: "9" indicates V7R1
