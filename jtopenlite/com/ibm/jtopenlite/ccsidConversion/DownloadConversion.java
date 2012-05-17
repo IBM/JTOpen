03b43bdd4dcce230d58b5f156d5cb246c79d7a09
John Eberhard <jeber@us.ibm.com>
2012-05-17 21:01:33 +0000

    jtopenlite:addition CCSID support

---
+++ jtopenlite/com/ibm/jtopenlite/ccsidConversion/DownloadConversion.java
@@ -0,0 +1,26 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// JTOpenLite
+//
+// Filename:  DownloadConversion.java
+//
+// The source code contained herein is licensed under the IBM Public License
+// Version 1.0, which has been approved by the Open Source Initiative.
+// Copyright (C) 2011-2012 International Business Machines Corporation and
+// others.  All rights reserved.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+package com.ibm.jtopenlite.ccsidConversion;
+
+public class DownloadConversion {
+	/* TODO */
+	public static SingleByteConversion downloadSingeByteConversion(String system,
+				String userid, 	String password, int ccsid) {
+
+		return null;
+
+
+	}
+
+}
---
+++ jtopenlite/com/ibm/jtopenlite/ccsidConversion/package-info.java
@@ -0,0 +1,6 @@
+
+/**
+ * <p>
+ * This package provides classes to convert between EBCDIC CCSIDS and unicode.
+ */
+package com.ibm.jtopenlite.ccsidConversion;
