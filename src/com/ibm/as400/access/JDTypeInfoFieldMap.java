664a1b8e85f924e44d027bc59150a831dce92280
John Eberhard <jeber@us.ibm.com>
2012-06-18 18:52:40 +0000

    JDBC:  DBCLOB getPrecision fix

--- src/com/ibm/as400/access/JDTypeInfoFieldMap.java
+++ src/com/ibm/as400/access/JDTypeInfoFieldMap.java
@@ -1,14 +1,14 @@
 ///////////////////////////////////////////////////////////////////////////////
-//                                                                             
-// JTOpen (IBM Toolbox for Java - OSS version)                                 
-//                                                                             
+//
+// JTOpen (IBM Toolbox for Java - OSS version)
+//
 // Filename: JDTypeInfoFieldMap.java
-//                                                                             
-// The source code contained herein is licensed under the IBM Public License   
-// Version 1.0, which has been approved by the Open Source Initiative.         
-// Copyright (C) 1997-2003 International Business Machines Corporation and     
-// others. All rights reserved.                                                
-//                                                                             
+//
+// The source code contained herein is licensed under the IBM Public License
+// Version 1.0, which has been approved by the Open Source Initiative.
+// Copyright (C) 1997-2003 International Business Machines Corporation and
+// others. All rights reserved.
+//
 ///////////////////////////////////////////////////////////////////////////////
 
 package com.ibm.as400.access;
@@ -27,10 +27,10 @@ implements JDFieldMap
     private int scaleIndex_;
     private int typeIndex_;
     private JDProperties properties_; // @M0A - added JDProperties so we can get the decimal scale & precision
-    private int vrm_;                 // @M0A - added vrm so we can pass it to the newData method 
+    private int vrm_;                 // @M0A - added vrm so we can pass it to the newData method
     private int ccsidIndex_;          //@KKB  added ccsid
 
-    JDTypeInfoFieldMap(int typeIndex, int lengthIndex, int precisionIndex, 
+    JDTypeInfoFieldMap(int typeIndex, int lengthIndex, int precisionIndex,
                        int scaleIndex, int ccsidIndex, int vrm, JDProperties properties) // @M0C - added vrm and properties //@KKB
     {
         typeIndex_          = typeIndex;
@@ -47,6 +47,10 @@ implements JDFieldMap
     {
         String typeName = row.getSQLData(typeIndex_).getString().trim();
         int length = row.getSQLData(lengthIndex_).getInt();
+        // If the type is DBCLOB / GRAPHIC / VARGRAPHIC  -- newData expects length in BYTES
+        if ("DBCLOB".equals(typeName) || "GRAPHIC".equals(typeName) || "VARG".equals(typeName) || "VARGRAPHIC".equals(typeName)   ) {
+          length = length * 2;
+        }
         int precision = row.getSQLData(precisionIndex_).getInt();
         int scale = row.getSQLData(scaleIndex_).getInt();
         int ccsid = 0;                                          //@KKB
