4eb3ce39b41a551d2132626b3902dcfd72651483
John Eberhard <jeber@us.ibm.com>
2012-08-27 13:10:35 +0000

    JDBC:  Fix array input parameter when reusing callable statement

--- src/com/ibm/as400/access/Copyright.java
+++ src/com/ibm/as400/access/Copyright.java
@@ -94,8 +94,8 @@ public interface Copyright
 {
     /** @deprecated  This field is reserved for use within the Toolbox product. **/
     public static String copyright = "Copyright (C) 1997-2011 International Business Machines Corporation and others.";
-    //                                                                                              built=20120820 is automatically set by the build (see build.xml)
-    public static String version   = "Open Source Software, JTOpen 7.7, codebase 5770-SS1 V7R1M0.07 built=20120820 @G4";  // As of V7R1: JC1 is merged into SS1 Option 3
+    //                                                                                              built=20120824 is automatically set by the build (see build.xml)
+    public static String version   = "Open Source Software, JTOpen 7.7, codebase 5770-SS1 V7R1M0.07 built=20120824 @G7";  // As of V7R1: JC1 is merged into SS1 Option 3
 
     // Constants for reference by AS400JDBCDriver.
     static final int    MAJOR_VERSION = 9; // ex: "9" indicates V7R1
