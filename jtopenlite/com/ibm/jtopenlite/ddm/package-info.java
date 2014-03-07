36fbcb64bda5edd9249b8cc90576745243116470
John Eberhard <jeber@us.ibm.com>
2014-03-07 18:54:09 +0000

    Javadoc updates

--- jtopenlite/com/ibm/jtopenlite/ddm/package-info.java
+++ jtopenlite/com/ibm/jtopenlite/ddm/package-info.java
@@ -3,7 +3,7 @@
  * Provides classes to access database files on an IBM i host using DDM.
  *
  *<p>
- *  The {@link com.ibm.jtopenlite.file.DDMConnection } class is used to establish
+ *  The {@link com.ibm.jtopenlite.ddm.DDMConnection } class is used to establish
  *  a connection to the host server (QUSRWRK/QRWTSRVR job).  
  *  See {@link com.ibm.jtopenlite.samples.DDMRead} for an example of using DDM to read a file. 
  */
