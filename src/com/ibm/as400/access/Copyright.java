3cb50ad2e00bad2163b01f05a62d3faf5941fdd8
John Eberhard <jeber@us.ibm.com>
2014-03-08 13:52:09 +0000

    Updated for JTOpen 8.2

--- src/com/ibm/as400/access/Copyright.java
+++ src/com/ibm/as400/access/Copyright.java
@@ -123,7 +123,7 @@ public interface Copyright
     /** @deprecated  This field is reserved for use within the Toolbox product. **/
     public static String copyright = "Copyright (C) 1997-2014 International Business Machines Corporation and others.";
     //                                                                                              built=20140307 is automatically set by the build (see build.xml)
-    public static String version   = "Open Source Software, JTOpen 8.2, codebase 5770-SS1 V7R2M0.00 built=20140307 @KD";  
+    public static String version   = "Open Source Software, JTOpen 8.2, codebase 5770-SS1 V7R2M0.00 built=20140308 @KD";  
 
     // Constants for reference by AS400JDBCDriver.
     static final int    MAJOR_VERSION = 10; // ex: "10" indicates V7R2, while "9" indicates V7R1
