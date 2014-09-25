b169ffadbf18d6fb3c3123e75f877aa9a165664a
John Eberhard <jeber@us.ibm.com>
2014-09-25 18:09:45 +0000

    JDBC: Support *ALLUSR schema name on DatabaseMetaData.getTables

--- src/com/ibm/as400/access/Copyright.java
+++ src/com/ibm/as400/access/Copyright.java
@@ -143,7 +143,7 @@ public interface Copyright
     /** @deprecated  This field is reserved for use within the Toolbox product. **/
     public static String copyright = "Copyright (C) 1997-2014 International Business Machines Corporation and others.";
     //                                                                                              built=20140516 is automatically set by the build (see build.xml)
-    public static String version   = "Open Source Software, JTOpen 8.4, codebase 5770-SS1 V7R2M0.00 built=20140821 @M2";  
+    public static String version   = "Open Source Software, JTOpen 8.4, codebase 5770-SS1 V7R2M0.00 built=20140825 @M3";  
 
     // Constants for reference by AS400JDBCDriver.
     static final int    MAJOR_VERSION = 10; // ex: "10" indicates V7R2, while "9" indicates V7R1
