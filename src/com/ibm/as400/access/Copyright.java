f4e7d3422004cb7e1a6772f49cb5cee39326045c
John Eberhard <jeber@us.ibm.com>
2014-12-04 20:37:00 +0000

    JDBC:  Fix default setting of schema in AS400JDBCPooledConnect for system naming

--- src/com/ibm/as400/access/Copyright.java
+++ src/com/ibm/as400/access/Copyright.java
@@ -140,6 +140,7 @@ package com.ibm.as400.access;
 //@M6    20141010  JDBC:  Allow use of sort sequence table in IASP
 //@M7    20141024  Misc:  Improve performance of RLE decompression
 //@M8    20141111  DDM:  Fix DRDA correlation id issue
+//@N1    20141204  JDBC:  Fix default setting of schema in AS400JDBCPooledConnect for system naming
 // NOTE:  When adding a line above, adjust the "String version" with the flag value.
 //--------------------------------------------------------------------
 
@@ -148,7 +149,7 @@ public interface Copyright
     /** @deprecated  This field is reserved for use within the Toolbox product. **/
     public static String copyright = "Copyright (C) 1997-2014 International Business Machines Corporation and others.";
     //                                                                                              built=20141114 is automatically set by the build (see build.xml)
-    public static String version   = "Open Source Software, JTOpen 8.4, codebase 5770-SS1 V7R2M0.00 built=20141114 @M8";  
+    public static String version   = "Open Source Software, JTOpen 8.4+, codebase 5770-SS1 V7R2M0.00 built=20141114 @N1";  
 
     // Constants for reference by AS400JDBCDriver.
     static final int    MAJOR_VERSION = 10; // ex: "10" indicates V7R2, while "9" indicates V7R1
