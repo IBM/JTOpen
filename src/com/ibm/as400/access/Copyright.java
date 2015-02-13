245a55a60b832689aac7fb33030e57b124f6d6c3
wanghuiq <wanghuiq>
2015-02-13 02:05:22 +0000

    Updates for jtopen 8.5

--- src/com/ibm/as400/access/Copyright.java
+++ src/com/ibm/as400/access/Copyright.java
@@ -145,6 +145,7 @@ package com.ibm.as400.access;
 //@N3    20150113  JDBC:  Fix CHAR FOR BIT DATA parameters in input variable field compression
 //@N4    20150119  Convert: update 16684, 300, 4396 conversion table  
 //@N5    20150211  DHCP connect to port with non-localhost  
+//@N6    20150213  Program Call: Support max 255 parameters in program call
 // NOTE:  When adding a line above, adjust the "String version" with the flag value.
 //--------------------------------------------------------------------
 
@@ -153,7 +154,7 @@ public interface Copyright
     /** @deprecated  This field is reserved for use within the Toolbox product. **/
     public static String copyright = "Copyright (C) 1997-2014 International Business Machines Corporation and others.";
     //                                                                                              built=20150210 is automatically set by the build (see build.xml)
-    public static String version   = "Open Source Software, JTOpen 8.4+, codebase 5770-SS1 V7R2M0.00 built=20150211 @N5";  
+    public static String version   = "Open Source Software, JTOpen 8.4+, codebase 5770-SS1 V7R2M0.00 built=20150213 @N6";  
 
     // Constants for reference by AS400JDBCDriver.
     static final int    MAJOR_VERSION = 10; // ex: "10" indicates V7R2, while "9" indicates V7R1
