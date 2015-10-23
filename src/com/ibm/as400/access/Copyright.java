924b7de954c9daab5212b37ffffddc09f1981120
wanghuiq <wanghuiq>
2015-10-23 01:42:10 +0000

    Update for jtopen 8.7

--- src/com/ibm/as400/access/Copyright.java
+++ src/com/ibm/as400/access/Copyright.java
@@ -170,12 +170,12 @@ public interface Copyright
     /** @deprecated  This field is reserved for use within the Toolbox product. **/
     public static String copyright = "Copyright (C) 1997-2015 International Business Machines Corporation and others.";
     //                                                                                              built=20151001 is automatically set by the build (see build.xml)
-    public static String version   = "Open Source Software, JTOpen 9.1, codebase 5770-SS1 V7R2M0.00 built=20151016 @P5";  
+    public static String version   = "Open Source Software, JTOpen 8.7, codebase 5770-SS1 V7R2M0.00 built=20151016 @P5";  
 
     // Constants for reference by AS400JDBCDriver.
-    static final int    MAJOR_VERSION = 11; // ex: "11" indicates V7R3, while "10" indicates V7R2
-    static final int    MINOR_VERSION = 1; // ex: "1" indicates PTF #1 (1 is first PTF in a release)
-                                           //Note: JTOpen 9.1 is synching with ptf 11.1
-    static final String DRIVER_LEVEL  = "07030001"; //(ex: 07030001 -> V7R3M0 PTF#1) (needed for hidden clientInfo) (each # is 2 digits in length)
+    static final int    MAJOR_VERSION = 10; // ex: "10" indicates V7R2, while "9" indicates V7R1
+    static final int    MINOR_VERSION = 7; // ex: "7" indicates PTF #7 (1 is first PTF in a release)
+                                           //Note: JTOpen 8.7 is synching with ptf 10.7
+    static final String DRIVER_LEVEL  = "07020007"; //(ex: 07020007 -> V7R2M0 PTF#7) (needed for hidden clientInfo) (each # is 2 digits in length)
 
 }
