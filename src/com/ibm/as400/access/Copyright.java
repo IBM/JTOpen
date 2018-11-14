b9827d4219bc8a12b826de52bfe339d6b76dc39c
Ze Zhang <zezhang@cn.ibm.com>
2018-11-14 10:00:28 +0000

    Support ASP API by zz.

--- src/com/ibm/as400/access/Copyright.java
+++ src/com/ibm/as400/access/Copyright.java
@@ -238,6 +238,7 @@ package com.ibm.as400.access;
 //@X5    20181024  JDBC:  Fix stored procedure call with null and array parameters
 //@X6    20181108  JDBC:  Fix looping when mixed character truncation occurs during batch insert
 //@X7    20181112  Fix incorrect time zone returned because a space at end of string.
+//@X8    20181114  Support ASP API 
 // NOTE:  When adding a line above, adjust the "String version" with the flag value.
 //--------------------------------------------------------------------
 
@@ -246,7 +247,7 @@ public interface Copyright
     /** @deprecated  This field is reserved for use within the Toolbox product. **/
     public static String copyright = "Copyright (C) 1997-2017 International Business Machines Corporation and others.";
     //                                                                                         built=20181031 is automatically set by the build (see build.xml)
-    public static String version   = "Open Source Software, JTOpen 9.6, codebase 5770-SS1 V7R3M0.00 built=20181108 @X6";  
+    public static String version   = "Open Source Software, JTOpen 9.6, codebase 5770-SS1 V7R3M0.00 built=20181108 @X8";  
 
 
     // Constants for reference by AS400JDBCDriver.
