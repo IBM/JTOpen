6658fdc345eaa3ad9f25523dd43c9a796173f36b
John Eberhard <jeber@us.ibm.com>
2018-11-16 17:16:58 +0000

    Support ASP API by zz

--- src/com/ibm/as400/access/list/AspOpenList.java
+++ src/com/ibm/as400/access/list/AspOpenList.java
@@ -1,3 +1,17 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// JTOpen (IBM Toolbox for Java - OSS version)
+//
+// Filename:  AspOpenList.java
+//
+// The source code contained herein is licensed under the IBM Public License
+// Version 1.0, which has been approved by the Open Source Initiative.
+// Copyright (C) 2018-2019 International Business Machines Corporation and
+// others.  All rights reserved.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+
 package com.ibm.as400.access.list;
 
 import java.io.IOException;
