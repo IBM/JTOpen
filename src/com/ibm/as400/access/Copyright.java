df0e0fd1b2fddab997377a4b2bcefb5ee72f2a89
Ze Zhang <zezhang@cn.ibm.com>
2019-09-02 02:33:34 +0000

    Update for JTOpen 10.1

--- src/com/ibm/as400/access/Copyright.java
+++ src/com/ibm/as400/access/Copyright.java
@@ -259,6 +259,7 @@ package com.ibm.as400.access;
 //@Z6    20190828  validateSignon timeout
 //@Z7    20190828  Fix stripping leading spaces from message text
 //@Z8    20190828  Show Technology refresh PTF for PTF
+//@AA1   20190902  JTOpen 10.1
 // NOTE:  When adding a line above, adjust the "String version" with the flag value.
 //--------------------------------------------------------------------
 
@@ -267,15 +268,15 @@ public interface Copyright
     /** @deprecated  This field is reserved for use within the Toolbox product. **/
     public static String copyright = "Copyright (C) 1997-2017 International Business Machines Corporation and others.";
     // Name of current release to be picked up by Java doc build. 
-    public static String JTOpenName = "JTOpen 9.8"; 
+    public static String JTOpenName = "JTOpen 10.1"; 
     //                                                                                         built=20190709 is automatically set by the build (see build.xml)
-    public static String version   = "Open Source Software, "+JTOpenName+" codebase 5770-SS1 V7R4M0.00 built=20190709 @Z5";  
+    public static String version   = "Open Source Software, "+JTOpenName+" codebase 5770-SS1 V7R4M0.00 built=20190709 @Z8";  
 
 
     // Constants for reference by AS400JDBCDriver.
-    static final int    MAJOR_VERSION = 11; // ex: "11" indicates V7R3, "10" indicates V7R2, while "9" indicates V7R1
-    static final int    MINOR_VERSION = 8; // ex: "1" indicates PTF #1 (1 is first PTF in a release)
+    static final int    MAJOR_VERSION = 12; // ex: "11" indicates V7R3, "10" indicates V7R2, while "9" indicates V7R1
+    static final int    MINOR_VERSION = 1; // ex: "1" indicates PTF #1 (1 is first PTF in a release)
                                            //Note: JTOpen 9.1 is synching with ptf 11.1
-    static final String DRIVER_LEVEL  = "07030008"; //(ex: 07030008 -> V7R3M0 PTF#8) (needed for hidden clientInfo) (each # is 2 digits in length)
+    static final String DRIVER_LEVEL  = "07040001"; //(ex: 07030008 -> V7R3M0 PTF#8) (needed for hidden clientInfo) (each # is 2 digits in length)
 
 }
