bdd807be6e0bc1bd26ebcb3c5dba04d0738541b5
John Eberhard <jeber@us.ibm.com>
2021-08-25 16:12:19 +0000

    Misc:  Code cleanup

--- src/com/ibm/as400/access/IFSOpenNodeRep.java
+++ src/com/ibm/as400/access/IFSOpenNodeRep.java
@@ -1,3 +1,16 @@
+///////////////////////////////////////////////////////////////////////////////
+//                                                                             
+// JTOpen (IBM Toolbox for Java - OSS version)                              
+//                                                                             
+// Filename: IFSOpenNodeRep.java
+//                                                                             
+// The source code contained herein is licensed under the IBM Public License   
+// Version 1.0, which has been approved by the Open Source Initiative.         
+// Copyright (C) 2021 International Business Machines Corporation and     
+// others. All rights reserved.                                                
+//                                                                             
+///////////////////////////////////////////////////////////////////////////////
+
 package com.ibm.as400.access;
 
 public class IFSOpenNodeRep extends IFSDataStream {
--- src/com/ibm/as400/access/IFSOpenNodeReq.java
+++ src/com/ibm/as400/access/IFSOpenNodeReq.java
@@ -1,6 +1,20 @@
+///////////////////////////////////////////////////////////////////////////////
+//                                                                             
+// JTOpen (IBM Toolbox for Java - OSS version)                              
+//                                                                             
+// Filename: IFSOpenNodeReq.java
+//                                                                             
+// The source code contained herein is licensed under the IBM Public License   
+// Version 1.0, which has been approved by the Open Source Initiative.         
+// Copyright (C) 2021 International Business Machines Corporation and     
+// others. All rights reserved.                                                
+//                                                                             
+///////////////////////////////////////////////////////////////////////////////
+
 package com.ibm.as400.access;
 
 public class IFSOpenNodeReq extends IFSDataStreamReq {
+  private static final String copyright = "Copyright (C) 2021 International Business Machines Corporation and others.";
 	private static final int HEADER_LENGTH = 20;
 	private static final int TEMPLATE_LENGTH = 14;
 	private static final int OBJECT_HANDLE_OFFSET = HEADER_LENGTH + 2;
