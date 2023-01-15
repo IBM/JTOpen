f04a97c7bb7da0e12f26591dde243c7c5ac9de51
Ze Zhang <zezhang@cn.ibm.com>
2023-01-15 08:54:42 +0000

    Updates for JTOpen 11.2

--- src/com/ibm/as400/access/Copyright.java
+++ src/com/ibm/as400/access/Copyright.java
@@ -337,15 +337,15 @@ public interface Copyright
     /** @deprecated  This field is reserved for use within the Toolbox product. **/
     public static String copyright = "Copyright (C) 1997-2020 International Business Machines Corporation and others.";
     // Name of current release to be picked up by Java doc build. 
-    public static String JTOpenName = "JTOpen 11.1"; 
+    public static String JTOpenName = "JTOpen 11.2"; 
     //                                                                                                 built=20221010 automatically set by the build (see build.xml)
-    public static String version   = "Open Source Software, "+JTOpenName+" codebase 5770-SS1 V7R5M0.00 built=20221010 @AI5";  
+    public static String version   = "Open Source Software, "+JTOpenName+" codebase 5770-SS1 V7R5M0.00 built=20221010 @AI7";  
 
 
     // Constants for reference by AS400JDBCDriver.
     static final int    MAJOR_VERSION = 13; // ex: "12" indicates V7R4, "10" indicates V7R2, while "9" indicates V7R1
-    static final int    MINOR_VERSION = 2; // ex: "1" indicates PTF #1 (1 is first PTF in a release)
+    static final int    MINOR_VERSION = 3; // ex: "1" indicates PTF #1 (1 is first PTF in a release)
                                            //Note: JTOpen 10.1 is synching with ptf 12.1
-    static final String DRIVER_LEVEL  = "07050002"; //(ex: 07030008 -> V7R3M0 PTF#8) (needed for hidden clientInfo) (each # is 2 digits in length)
+    static final String DRIVER_LEVEL  = "07050003"; //(ex: 07030008 -> V7R3M0 PTF#8) (needed for hidden clientInfo) (each # is 2 digits in length)
 
 }
