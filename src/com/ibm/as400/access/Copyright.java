baba931e5f4838ff88ea29fddc007944eb25c851
Ze Zhang <zezhang@cn.ibm.com>
2023-02-17 05:35:56 +0000

    Update the flag

--- src/com/ibm/as400/access/Copyright.java
+++ src/com/ibm/as400/access/Copyright.java
@@ -341,7 +341,7 @@ public interface Copyright
     // Name of current release to be picked up by Java doc build. 
     public static String JTOpenName = "JTOpen 11.2"; 
     //                                                                                                 built=20221010 automatically set by the build (see build.xml)
-    public static String version   = "Open Source Software, "+JTOpenName+" codebase 5770-SS1 V7R5M0.00 built=20221010 @AI7";  
+    public static String version   = "Open Source Software, "+JTOpenName+" codebase 5770-SS1 V7R5M0.00 built=20221010 @AI9";  
 
 
     // Constants for reference by AS400JDBCDriver.
