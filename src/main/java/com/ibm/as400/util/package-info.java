c15490ed94a1ad24a2343f10a5012e1986d3c9dc
Nicola Isotta <nicola.isotta@gmail.com>
2023-11-03 10:36:50 +0100

    Fix javadoc generation (#126)
    Signed-off-by: Nicola Isotta <nicola.isotta@gmail.com>

---
+++ src/main/java/com/ibm/as400/util/package-info.java
@@ -0,0 +1,19 @@
+/**
+ * Provides various utility applications and classes.
+ *
+ * <P>
+ * This package currently provides the following utilities:
+ * <UL>
+ * <LI><B>AboutToolbox</B> - Prints the current version of the Toolbox that is
+ * found in the user's CLASSPATH.
+ * <LI><B>CommandHelpRetriever</B> - Allows you to generate HTML documentation
+ * for a given CL command (*CMD) name, library, and system. This function is
+ * available both as a Java program and as an application programming interface
+ * (API).
+ * <LI><B>JPing</B> - Allows you to remotely determine if the host server jobs
+ * that the Toolbox uses for communication are started and running on your IBM i
+ * system.
+ *
+ * </UL>
+ */
+package com.ibm.as400.util;
---
+++ src/main/java/utilities/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Provides various utility applications and classes.
+ * <p>
+ * <i>Warning: The JPing and AboutToolbox classes have been moved to
+ * com.ibm.as400.util. This "utility" package is deprecated and will be removed
+ * from the Java 9 version of the jar file.</i>
+ * </p>
+ */
+package utilities;
