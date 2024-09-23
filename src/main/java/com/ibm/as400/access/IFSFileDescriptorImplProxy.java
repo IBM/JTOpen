abaf8b8fabb09e6ca74d8191a1b567ae1a16f31f
Nadir K Amra <amra@us.ibm.com>
2024-09-23 14:14:20 -0500

    IFSFile fixes, and some simplification/reformatting
    Signed-off-by: Nadir K Amra <amra@us.ibm.com>

--- src/main/java/com/ibm/as400/access/IFSFileDescriptorImplProxy.java
+++ src/main/java/com/ibm/as400/access/IFSFileDescriptorImplProxy.java
@@ -16,125 +16,107 @@ package com.ibm.as400.access;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 
-
 /**
- Provides a local proxy implementation for the IFSFileDescriptor class.
+ * Provides a local proxy implementation for the IFSFileDescriptor class.
  **/
-class IFSFileDescriptorImplProxy
-extends AbstractProxyImpl
-implements IFSFileDescriptorImpl
+class IFSFileDescriptorImplProxy extends AbstractProxyImpl implements IFSFileDescriptorImpl
 {
-  private static final String copyright = "Copyright (C) 1997-2000 International Business Machines Corporation and others.";
+    private static final String copyright = "Copyright (C) 1997-2000 International Business Machines Corporation and others.";
 
-  
+    IFSFileDescriptorImplProxy() {
+        super("IFSFileDescriptor");
+    }
 
-  IFSFileDescriptorImplProxy ()
-  {
-    super ("IFSFileDescriptor");
-  }
+    @Override
+    public void close()
+    {
+        try {
+            connection_.callMethod(pxId_, "close");
+        } 
+        catch (InvocationTargetException e) {
+            throw ProxyClientConnection.rethrow(e);
+        }
+    }
 
-  public void close()
-  {
-    try {
-      connection_.callMethod (pxId_, "close");
+    @Override
+    public int getCCSID() throws IOException
+    {
+        try {
+            return connection_.callMethod(pxId_, "getCCSID").getReturnValueInt();
+        }
+        catch (InvocationTargetException e) {
+            throw ProxyClientConnection.rethrow(e);
+        }
     }
-    catch (InvocationTargetException e) {
-      throw ProxyClientConnection.rethrow (e);
-    }
-  }
 
-  public int getCCSID() throws IOException
-  {
-    try {
-      return connection_.callMethod (pxId_, "getCCSID")
-                        .getReturnValueInt();
+    @Override
+    public long getFileOffset()
+    {
+        try {
+            return connection_.callMethod(pxId_, "getFileOffset").getReturnValueLong();
+        }
+        catch (InvocationTargetException e) {
+            throw ProxyClientConnection.rethrow(e);
+        }
     }
-    catch (InvocationTargetException e) {
-      throw ProxyClientConnection.rethrow (e);
-    }
-  }
 
-  public long getFileOffset()
-  {
-    try {
-      return connection_.callMethod (pxId_, "getFileOffset")
-                        .getReturnValueLong();
+    @Override
+    public void incrementFileOffset(long fileOffsetIncrement)
+    {
+        try {
+            connection_.callMethod(pxId_, "incrementFileOffset", new Class[] { Long.TYPE },  new Object[] { Long.valueOf(fileOffsetIncrement) });
+        } 
+        catch (InvocationTargetException e) {
+            throw ProxyClientConnection.rethrow(e);
+        }
     }
-    catch (InvocationTargetException e) {
-      throw ProxyClientConnection.rethrow (e);
-    }
-  }
 
-  public void incrementFileOffset(long fileOffsetIncrement)
-  {
-    try {
-      connection_.callMethod (pxId_, "incrementFileOffset",
-                              new Class[] { Long.TYPE },
-                              new Object[] { Long.valueOf(fileOffsetIncrement)});
+    @Override
+    public void initialize(long fileOffset, Object parentImpl, String path, int shareOption, AS400Impl system)
+    {
+        try {
+            connection_.callMethod(pxId_, "initialize",
+                    new Class[] { Long.TYPE, Object.class, String.class, Integer.TYPE, AS400Impl.class },
+                    new Object[] { Long.valueOf(fileOffset), parentImpl, path, Integer.valueOf(shareOption), system });
+        }
+        catch (InvocationTargetException e) {
+            throw ProxyClientConnection.rethrow(e);
+        }
     }
-    catch (InvocationTargetException e) {
-      throw ProxyClientConnection.rethrow (e);
-    }
-  }
 
-  public void initialize(long fileOffset, Object parentImpl, String path, int shareOption,
-                         AS400Impl system)
-  {
-    try {
-      connection_.callMethod (pxId_, "initialize",
-                              new Class[] { Long.TYPE,
-                                            Object.class,
-                                            String.class,
-                                            Integer.TYPE,
-                                            AS400Impl.class
-                                          },
-                              new Object[] { Long.valueOf(fileOffset),
-                                            parentImpl,
-                                             path,
-                                             Integer.valueOf(shareOption),
-                                             system
-                                           });
+    @Override
+    public boolean isOpen()
+    {
+        try {
+            return connection_.callMethod(pxId_, "isOpen").getReturnValueBoolean();
+        }
+        catch (InvocationTargetException e) {
+            throw ProxyClientConnection.rethrow(e);
+        }
     }
-    catch (InvocationTargetException e) {
-      throw ProxyClientConnection.rethrow (e);
-    }
-  }
 
-  public boolean isOpen()
-  {
-    try {
-      return connection_.callMethod (pxId_, "isOpen")
-                        .getReturnValueBoolean();
+    @Override
+    public void setFileOffset(long fileOffset)
+    {
+        try {
+            connection_.callMethod(pxId_, "setFileOffset", new Class[] { Long.TYPE }, new Object[] { Long.valueOf(fileOffset) });
+        }
+        catch (InvocationTargetException e) {
+            throw ProxyClientConnection.rethrow(e);
+        }
     }
-    catch (InvocationTargetException e) {
-      throw ProxyClientConnection.rethrow (e);
-    }
-  }
 
-  public void setFileOffset(long fileOffset)
-  {
-    try {
-      connection_.callMethod (pxId_, "setFileOffset",
-                              new Class[] { Long.TYPE },
-                              new Object[] { Long.valueOf(fileOffset)});
+    @Override
+    public void sync() throws IOException
+    {
+        try {
+            connection_.callMethod(pxId_, "sync");
+        }
+        catch (InvocationTargetException e) {
+            throw ProxyClientConnection.rethrow1(e);
+        }
     }
-    catch (InvocationTargetException e) {
-      throw ProxyClientConnection.rethrow (e);
-    }
-  }
-
-  public void sync() throws IOException
-  {
-    try {
-      connection_.callMethod (pxId_, "sync");
-    }
-    catch (InvocationTargetException e) {
-      throw ProxyClientConnection.rethrow1 (e);
-    }
-  }
-
 }
-
 
 
 
