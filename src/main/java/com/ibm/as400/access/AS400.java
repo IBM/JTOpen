2b5824e7150072af9296a8f05733cde93f70f4a6
John Eberhard <jeber@us.ibm.com>
2026-03-03 13:37:36 -0600

    Do not throw exception if SocketProperties are the same
    Signed-off-by: John Eberhard <jeber@us.ibm.com>

--- src/main/java/com/ibm/as400/access/AS400.java
+++ src/main/java/com/ibm/as400/access/AS400.java
@@ -5291,10 +5291,12 @@ public class AS400 implements Serializable, AutoCloseable
         if (socketProperties == null)
             throw new NullPointerException("socketProperties");
 
-        if (propertiesFrozen_)
-        {
+        if (propertiesFrozen_) {
+          // If no change is being made, don't throw exception 
+          if (! socketProperties_.equals(socketProperties)) {
             Trace.log(Trace.ERROR, "Cannot set socket properties after connection has been made.");
             throw new ExtendedIllegalStateException("socketProperties", ExtendedIllegalStateException.PROPERTY_NOT_CHANGED);
+          }
         }
         socketProperties_.copyValues(socketProperties);
     }
